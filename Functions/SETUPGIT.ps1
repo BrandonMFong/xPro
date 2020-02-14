b433b9e446718946ccdfdf750b89a515bad3b374
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-02-13 20:49:18 -0800

    Edit Profile, edit config

--- Functions/SETUPGIT.ps1
+++ Functions/SETUPGIT.ps1
@@ -1,13 +1,13 @@
 #Since my Microsoft Powershell profile is originally in this directory, I call this script to copy the script so I can push to the git repo
 try
 {
-    GO-TO PowershellRepoPath -p;
+    goto PowershellRepoPath -p;
         Set-Location Profile;
         $FileName="Pass"; # Check function appendate file
         Get-ChildItem | 
             Where-Object{$_.Name -eq "Microsoft.PowerShell_profile.ps1"} | 
                 ForEach-Object{$FileName = $_};
-        Append-Date -FileName $FileName; #appending today's date 
+        Append -FileName $FileName; #appending today's date 
         if((Test-Path .\archive\) -AND ((Get-ChildItem .\archive\).count -gt 10)){Archive -Zip;}# TODO this puts the appended profile outside
         else{Archive;}
         Copy-Item $Profile .;

e3bf8b763fd41607c36a5513c5df8fb32635f816
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-03-18 21:56:43 -0700

    [SQUASH MERGE dev <= feature-GotoDynamicallyAddADirectory]
    commit c05b6dd5d3706f69fbf4774eaac65e2855f7c0f3
    Author: Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
    Date:   Wed Mar 18 21:55:44 2020 -0700
    
        edit commit
    
    commit b78b4559ed24c6a2fbefb34d390e71a2388d1fca
    Author: Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
    Date:   Wed Mar 18 21:39:11 2020 -0700
    
        Adding method to goto.ps1

--- Functions/GOTO.ps1
+++ Functions/GOTO.ps1
@@ -1,7 +1,23 @@
 
-Param([String[]] $dir, [Alias('p')][Switch] $push )
-#[xml]$x = Get-Content $ConfigFile;
+Param([String[]] $dir, [Alias('p')][Switch] $push, [switch]$AddDirectory)
 [bool]$ProcessExecuted = $false;
+
+if($AddDirectory)
+{
+	$PathToAdd = (Get-Location).path; # Get the directory you are adding
+	$XMLFile = $env:COMPUTERNAME.ToString() + ".xml"; 
+	Push-Location $PSScriptRoot;
+		Get-ChildItem $('..\Config\' + $XMLFile) |
+			ForEach-Object {$PathToXMLFile = $_.FullName;} # Get path to xml file
+		[xml]$x = Get-Content $PathToXMLFile;
+		$add = $x.CreateElement("Directory"); 
+		$Alias = Read-Host -Prompt "Set Alias";
+		$add.SetAttribute("alias", $Alias);
+		$add.InnerXml = $PathToAdd;
+		$x.Machine.Directories.AppendChild($add);
+		$x.Save($PathToXMLFile);
+	Pop-Location;
+}
 	
 foreach ($Directory in $XMLReader.Machine.Directories.Directory)
 {

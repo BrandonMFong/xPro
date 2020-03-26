e2977c5bad9caa2e660b3bfd1b77e22905e8f8fb
Brandon Fong <brandon.fong@Verint.com>
2020-03-26 11:29:25 -0700

    Auto externalID for tc table

--- SQLQueries/TableUpdates/TypeContent.sql
+++ SQLQueries/TableUpdates/TypeContent.sql
@@ -1,5 +1,10 @@
 begin transaction 
 -- New Column ExternalID
 alter table typecontent add ExternalID varchar(50) null
+
+update tc 
+set tc.ExternalID = REPLACE(description, ' ', '')
+from TypeContent tc 
+select * from TypeContent
 rollback transaction 
 --commit transaction

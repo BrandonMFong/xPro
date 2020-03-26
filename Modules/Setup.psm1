f87ba9a433593e9684bec65e465188d542739258
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-03-26 16:45:36 -0700

    Writing out functions

--- Modules/Setup.psm1
+++ Modules/Setup.psm1
@@ -1,7 +1,56 @@
-function GetConfigName
-{  
-    Param($n)
-    [string]$BaseName = Read-Host -Prompt "Config name (input 'q' for your computer name)";
-    if($BaseName -eq "q"){return $env:COMPUTERNAME.ToString();}
-    else {return $BaseName;}
+
+function MakeXMLFile
+{
+    [XML]$File = [XML]::new();
+    $Node_Machine = $File.CreateElement("Machine");
+    $Node_Machine.SetAttribute("MachineName",$env:COMPUTERNAME);
+    $Node_GitRepoDir = $File.CreateElement("GitRepoDir");
+    $Node_ConfigFile = $File.CreateElement("ConfigFile");
+    $File.AppendChild($Node_Machine);
+    $File.Machine.AppendChild($Node_GitRepoDir)
+    $File.Machine.AppendChild($Node_ConfigFile)
+    $File.Machine.GitRepoDir = $($PSScriptRoot | Split-Path -Parent).ToString();
+    $File.Machine.ConfigFile = $($env:COMPUTERNAME.ToString() + ".xml");
+    return $File;
 }
+
+function GetContents # static
+{
+    Param([xml]$x)
+    Write-Host("Machine Name : $($x.Machine.MachineName)");
+    Write-Host("Git Repository Directory : $($x.Machine.GitRepoDir)");
+    Write-Host("Configuration File : $($x.Machine.GitRepoDir)");
+}
+function InitPointer
+{
+    while(($x -ne 'y') -and ($x -ne 'n'))
+    {
+        $x = Read-Host -Prompt "Do you want to create your own config (y) or use another in the directory(n)?"
+    }
+
+    if ($x -eq 'y')
+    {
+        [XML]$NewXml = MakeXMLFile;
+        GetContents($NewXml);
+        if($(Read-Host -Prompt "Approve? (y/n)") -eq "y")
+        {
+            $NewXml.Save($($PRFOILE | Split-Path -Parent).ToString() + "\Profile.xml");
+        }
+        else {throw "Please restart setup then."} # maybe call this function again
+    }
+    else 
+    {
+        [Xml]$NewXml = MakeXMLFile;
+        $NewXml.Save($($PRFOILE | Split-Path -Parent).ToString() + "\Profile.xml");
+        .\update-config.ps1;
+    }
+}
+
+function InitProfile
+{
+    if(Test-Path $Profile)
+    {
+        New-Item –Path $Profile –Type File –Force
+    }
+}
+
--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,9 @@ Push-Location $PSScriptRoot;
     Import-Module .\Modules\Setup.psm1;
     
     Write-Host "`n`n Initializing setup`n`n";
-    $ConfigFileName = $(GetConfigName) + ".xml";
+
+    InitProfile;
+
+    InitPointer;
 
 Pop-Location;

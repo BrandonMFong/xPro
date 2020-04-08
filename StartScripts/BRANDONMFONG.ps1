df543795d1b2d8e3567b11c90e5024061692ef40
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-04-07 20:15:11 -0700

    [CONFIG] Fix config bug and switch statement at start script
    [LOG] Logging 1.11.0

--- StartScripts/BRANDONMFONG.ps1
+++ StartScripts/BRANDONMFONG.ps1
@@ -15,3 +15,14 @@ $x =
     Sunday = $SunList
 } 
 New-Variable -Name "Todo" -Value $x -Scope Global -Force;
+switch ((Get-Date).DayOfWeek)
+{
+    "Monday" {$Todo.Monday.ListOut()}
+    "Tuesday" {$Todo.Tuesday.ListOut()}
+    "Wednesday" {$Todo.Wednesday.ListOut()}
+    "Thursday" {$Todo.Thursday.ListOut()}
+    "Friday" {$Todo.Friday.ListOut()}
+    "Saturday" {$Todo.Saturday.ListOut()}
+    "Sunday" {$Todo.Sunday.ListOut()}
+    default{Write-Host "`n";}
+}

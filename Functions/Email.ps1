2a659b964206497381b2f31d00bd34d4430358db
Brandon Fong <brandon.fong@kiran.com>
2020-04-21 20:12:21 -0700

    Making email script configurable

--- Functions/Email.ps1
+++ Functions/Email.ps1
@@ -9,6 +9,7 @@
 
 Param([Switch]$Count, [Switch]$ListMessages,[Switch]$GetObject,[Switch]$GetBody,[int]$index=0)
 Import-Module $($PSScriptRoot + "\..\Modules\FunctionModules.psm1") -Scope Local;
+$x = $(Get-Variable "XMLReader").Value.Machine.Email;
 $inbox = InboxObject;
 if($ListMessages)
 {
@@ -18,15 +19,37 @@ if($ListMessages)
         for($i=0;$i -lt $inbox.Items.Count;$i++)
         {
             Write-Host `n;
-            Write-Host "From: " -ForegroundColor Cyan -NoNewline;
-            Write-Host "$(($inbox.Items|Select-Object -Property SenderName|Select-Object -Index $($i)).SenderName)";
-            Write-Host "Subject: " -ForegroundColor Cyan -NoNewline; 
-            Write-Host "$(($inbox.Items|Select-Object -Property Subject|Select-Object -Index $($i)).Subject)" ;
-            Write-Host "Time: " -ForegroundColor Cyan -NoNewline; 
-            $Time = ($inbox.Items|Select-Object -Property ReceivedTime|Select-Object -Index $($i)).ReceivedTime;
-            Write-Host "$(Get-Date $Time -Format "MM/dd/yyyy hh:mm tt")" ;
-            Write-Host "Body: " -ForegroundColor Cyan;
-            Write-Host "$(($inbox.Items|Select-Object -Property Body|Select-Object -Index $($i)).Body)" ;
+            if($x.IncludeFields.To -eq "True")
+            {
+                Write-Host "To: " -ForegroundColor Cyan -NoNewline;
+                Write-Host "$(($inbox.Items|Select-Object -Property To|Select-Object -Index $($i)).To)";
+            }
+            if($x.IncludeFields.CC -eq "True")
+            {
+                Write-Host "CC: " -ForegroundColor Cyan -NoNewline;
+                Write-Host "$(($inbox.Items|Select-Object -Property CC|Select-Object -Index $($i)).CC)";
+            }
+            if($x.IncludeFields.From -eq "True")
+            {
+                Write-Host "From: " -ForegroundColor Cyan -NoNewline;
+                Write-Host "$(($inbox.Items|Select-Object -Property SenderName|Select-Object -Index $($i)).SenderName)";
+            }
+            if($x.IncludeFields.Subject -eq "True")
+            {
+                Write-Host "Subject: " -ForegroundColor Cyan -NoNewline; 
+                Write-Host "$(($inbox.Items|Select-Object -Property Subject|Select-Object -Index $($i)).Subject)" ;
+            }
+            if($x.IncludeFields.Time -eq "True")
+            {
+                Write-Host "Time: " -ForegroundColor Cyan -NoNewline; 
+                $Time = ($inbox.Items|Select-Object -Property ReceivedTime|Select-Object -Index $($i)).ReceivedTime;
+                Write-Host "$(Get-Date $Time -Format "MM/dd/yyyy hh:mm tt")" ;
+            }
+            if($x.IncludeFields.Body -eq "True")
+            {
+                Write-Host "Body: " -ForegroundColor Cyan;
+                Write-Host "$(($inbox.Items|Select-Object -Property Body|Select-Object -Index $($i)).Body)" ;
+            }
             Write-Host "------------------------------------------------------------------------------------------";
         }
         Write-Host `n;
@@ -38,19 +61,37 @@ if($index -ne 0)
 {
     Write-Host `n;
     Write-Host "----------------------------------------------Start Message--------------------------------------------";
-    Write-Host "To: " -ForegroundColor Cyan -NoNewline;
-    Write-Host "$(($inbox.Items|Select-Object -Property To|Select-Object -Index $($index-1)).To)";
-    Write-Host "CC: " -ForegroundColor Cyan -NoNewline;
-    Write-Host "$(($inbox.Items|Select-Object -Property CC|Select-Object -Index $($index-1)).CC)";
-    Write-Host "From: " -ForegroundColor Cyan -NoNewline;
-    Write-Host "$(($inbox.Items|Select-Object -Property SenderName|Select-Object -Index $($index-1)).SenderName)";
-    Write-Host "Subject: " -ForegroundColor Cyan -NoNewline; 
-    Write-Host "$(($inbox.Items|Select-Object -Property Subject|Select-Object -Index $($index-1)).Subject)" ;
-    Write-Host "Time: " -ForegroundColor Cyan -NoNewline; 
-    $Time = ($inbox.Items|Select-Object -Property ReceivedTime|Select-Object -Index $($index-1)).ReceivedTime;
-    Write-Host "$(Get-Date $Time -Format "MM/dd/yyyy hh:mm tt")" ;
-    Write-Host "Body: " -ForegroundColor Cyan;
-    Write-Host "$(($inbox.Items|Select-Object -Property Body|Select-Object -Index $($index-1)).Body)" ;
+    if($x.IncludeFields.To -eq "True")
+    {
+        Write-Host "To: " -ForegroundColor Cyan -NoNewline;
+        Write-Host "$(($inbox.Items|Select-Object -Property To|Select-Object -Index $($iindex-1)).To)";
+    }
+    if($x.IncludeFields.CC -eq "True")
+    {
+        Write-Host "CC: " -ForegroundColor Cyan -NoNewline;
+        Write-Host "$(($inbox.Items|Select-Object -Property CC|Select-Object -Index $($iindex-1)).CC)";
+    }
+    if($x.IncludeFields.From -eq "True")
+    {
+        Write-Host "From: " -ForegroundColor Cyan -NoNewline;
+        Write-Host "$(($inbox.Items|Select-Object -Property SenderName|Select-Object -Index $($iindex-1)).SenderName)";
+    }
+    if($x.IncludeFields.Subject -eq "True")
+    {
+        Write-Host "Subject: " -ForegroundColor Cyan -NoNewline; 
+        Write-Host "$(($inbox.Items|Select-Object -Property Subject|Select-Object -Index $($iindex-1)).Subject)" ;
+    }
+    if($x.IncludeFields.Time -eq "True")
+    {
+        Write-Host "Time: " -ForegroundColor Cyan -NoNewline; 
+        $Time = ($inbox.Items|Select-Object -Property ReceivedTime|Select-Object -Index $($iindex-1)).ReceivedTime;
+        Write-Host "$(Get-Date $Time -Format "MM/dd/yyyy hh:mm tt")" ;
+    }
+    if($x.IncludeFields.Body -eq "True")
+    {
+        Write-Host "Body: " -ForegroundColor Cyan;
+        Write-Host "$(($inbox.Items|Select-Object -Property Body|Select-Object -Index $($iindex-1)).Body)" ;
+    }
     Write-Host "----------------------------------------------End Message-----------------------------------------------";
     Write-Host `n;
     break;
@@ -59,9 +100,11 @@ if($Count){return ($inbox.Items|Measure-Object).Count;}
 if($GetObject){return $inbox;}
 else
 {
+    if([string]::IsNullOrEmpty($x.ListInboxMax)){$Max = $x.ListInboxMax;}
+    else{$Max = $inbox.Items.Count;}
     if(($inbox.Items|Measure-Object).Count -gt 0)
     {
-        for($i=0;$i -lt $inbox.Items.Count;$i++)
+        for($i=0;$i -lt $Max;$i++)
         {
             Write-Host "{$($i+1)} " -NoNewline -ForegroundColor Cyan;
             Write-Host "[" -NoNewline -ForegroundColor Cyan;

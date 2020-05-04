b17a731dd847f42c7848e9b76b148091db18e7f9
Brandon Fong <brandon.fong@kiran.com>
2020-05-04 11:38:17 -0700

    moving outlook type

--- Modules/FunctionModules.psm1
+++ Modules/FunctionModules.psm1
@@ -269,15 +269,6 @@ function GetTCExtID([string]$Type)
     return $str;
 }
 
-Add-Type -assembly "Microsoft.Office.Interop.Outlook";
-function InboxObject
-{
-    $Outlook = New-Object -comobject Outlook.Application;
-    $namespace = $Outlook.GetNameSpace("MAPI");
-    $inbox = $namespace.GetDefaultFolder([Microsoft.Office.Interop.Outlook.OlDefaultFolders]::olFolderInbox)
-    return $inbox;
-}
-
 function Test-KeyPress
 {
     param

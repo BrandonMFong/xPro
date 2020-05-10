23543f0aa670ae0a6dfd1406272e132a71f21086
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-05-10 14:13:12 -0700

    Adding Copy-Folder Method but method already exists with -recurse in copy-item

--- Functions/Copy-Folder.ps1
+++ Functions/Copy-Folder.ps1
@@ -1,73 +1,46 @@
 <#
 .Synopsis
-
+    Copies FolderA to FolderB
 .Description
-
+    Depracated, can already use Copy-Item but good method
 .Notes
-    I'm assuming the paths provided in the params do not start with \
-    How to make it stop at the base dir
+    You can also use Copy-Item -recurse 
 #>
-Param
+Param 
 (
-    [string]$f="null",
-    [string]$d="null"
+    [Parameter(Mandatory=$true)][string]$FolderA = $null,
+    [Parameter(Mandatory=$true)][string]$FolderB = $null
 )
 
-class Folder 
+# Get all filepaths
+[System.Object[]]$AllFolderPathsFormer = (Get-ChildItem ($FolderA) -Recurse | Where-Object{$_.Attributes -notlike "*Directory*"}|ForEach-Object {$_.FullName});
+
+# Get old full base path
+Push-Location $FolderA;
+    [string]$OldBasePath = (Get-Location).Path;
+Pop-Location;
+# Get New full base path
+Push-Location $FolderB;
+    [string]$NewBasePath = (Get-Location).Path + "\$($FolderA | Split-Path -Leaf)";
+Pop-Location;
+
+[System.Object[]]$AllFolderPathsLadder = [system.object[]]::new($AllFolderPathsFormer.Length); # Declare new object
+for([int]$i = 0; $i -lt $AllFolderPathsFormer.Length;$i++)
 {
-    [object[]]$FullPaths;
-    [string]$BaseDir
-    [string]$CommonPath = "";
-
-    Folder([Object[]]$Paths,[string]$BaseDir)
-    {
-        $this.FullPaths = $Paths;
-        $this.BaseDir = $BaseDir;
-    }
-
-    [void]FindCommonPath() # I don't think I need this for this function but it is probably a good method to save
-    {
-        $common = $false;
-        for($i = 0;$i -lt $this.FullPaths[0].Length;$i++)# comparing against the first 
-        {
-            for($j = 1; $j -lt $this.FullPaths.Length;$j++)
-            {
-                if($this.FullPaths[0][$i] -eq $this.FullPaths[$j][$i])
-                {
-                    $common = $true;
-                }
-                else{$common = $false;}
-            }
-            if($common){$this.CommonPath = $this.CommonPath + $this.FullPaths[0][$i];}
-            else{break;}
-        }
-    }
-
-    [void]CopyOver([string]$NewBaseDir)
-    {
-        foreach($path in $this.FullPaths)
-        {
-            [string]$p = $path.ToString();
-            Copy-Item $p $p.Replace($this.BaseDir,$NewBaseDir);
-        }
-    }
+    $AllFolderPathsLadder[$i] = $AllFolderPathsFormer[$i].Replace($OldBasePath,$NewBasePath);
+    Write-Host "REWRITE: $($AllFolderPathsFormer[$i]) => $($AllFolderPathsLadder[$i])" -BackgroundColor Black -ForegroundColor Yellow;
+    Write-Progress -Activity "Replacing old base path to new base path" -status "New Path: $($AllFolderPathsLadder[$i])" -PercentComplete ((($i+1) / $AllFolderPathsFormer.Length)*100);
 }
+Write-Progress -Activity "Replacing old base path to new base path" -Completed;
 
-$f = $f.Replace('.','');
-$d = $d.Replace('.','');
-if (!(Test-Path $((Get-Location).Path + $f))){throw "$($f) does not exist."}
-if (!(Test-Path $((Get-Location).Path + $d))){throw "$($d) does not exist."}
-
-
-if(("null" -ne $f) -and ("null" -ne $d))
+for([int]$i = 0; $i -lt $AllFolderPathsLadder.Length;$i++)
 {
-    [System.Object[]]$APaths = (Get-ChildItem -Recurse ((get-location).Path + $f)).FullName;
-    [string]$ABaseDir = (((Get-Location).Path + $f) | Split-Path -Parent);
-    $AFolder = [Folder]::new($APaths,$ABaseDir); 
-    $BFolder = ((Get-Location).Path + $d).FullName; # Gets the full directory of the folder you are trying to copying to
-    $AFolder.CopyOver($BFolder);
+    New-Item -Force $AllFolderPathsLadder[$i];
+    Copy-Item $AllFolderPathsFormer[$i] $AllFolderPathsLadder[$i] -Recurse -Force;
+    Write-Host "COPY-ITEM: $($AllFolderPathsFormer[$i]) => $($AllFolderPathsLadder[$i])" -BackgroundColor Black -ForegroundColor Yellow;
+    Write-Progress -Activity "Copy Item" -status "Item: $($AllFolderPathsLadder[$i] | Split-Path -Leaf)" -PercentComplete ((($i+1) / $AllFolderPathsLadder.Length)*100);
 }
-else 
-{
-    throw "Cannot move $($f) to $($d).";
-}
+Write-Progress -Activity "Replacing old base path to new base path" -Completed;
+
+# For large amounts of items, not all items are copied.  Probably have to do with attribute type
+Write-Host "`nCopied $([Math]::Round(((Get-ChildItem $($FolderB + "$($FolderA | Split-Path -Leaf)") -Recurse | Measure-Object).Count / (Get-ChildItem $FolderA -Recurse | Measure-Object).Count) * 100, 2))% items!" -ForegroundColor Green;

7a136087c450ef8cab5b0c86a2c3a05b624d6bd6
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-06-07 11:39:27 -0700

    [Code] Change where credential check is on profile script

--- Profile.ps1
+++ Profile.ps1
@@ -7,38 +7,42 @@ Push-Location $($PROFILE |Split-Path -Parent);
 Pop-Location
 [XML]$XMLReader = Get-Content $($AppPointer.Machine.GitRepoDir + "\Config\" + $AppPointer.Machine.ConfigFile);
 
-# Get credentials
-# Should this be in the beginning?
-if($XMLReader.Machine.Secure.ToBoolean($null) -and !$LoggedIn)
-{
-    $cred = Get-Content ($AppPointer.Machine.GitRepoDir + "\bin\credentials\user.JSON") | ConvertFrom-Json  
-    [string]$user = Read-Host -prompt "Username"; 
-
-    # Get Secure string and then convert it back to plain text
-    [System.Object]$var = Read-Host -prompt "Password" -AsSecureString; 
-    [System.ValueType]$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($var)
-    [String]$password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
-    
-    if(($user -ne $cred.Username) -or ($cred.Password -ne $password))
-    {
-        Write-Error "WRONG CREDENTIALS";
-        exit;
-    }
-    else{[Boolean]$LoggedIn = $true;}
-}
-
 if(!$XMLReader.Machine.LoadProfile.ToBoolean($null)){break;} # Flag to load profile (in case someone wanting to use powershell)
 if($XMLReader.Machine.LoadProcedure -eq "Verbose"){[bool]$Verbose = $true} # Helps debugging if on
 else{[bool]$Verbose = $false}
 
 Push-Location $AppPointer.Machine.GitRepoDir; 
     Import-Module .\Modules\FunctionModules.psm1 -DisableNameChecking -Scope Local;
-    Import-Module .\Modules\Terminal.psm1 -DisableNameChecking -Scope Local;
-    
-    _SetBackgroundColor;
 
     <### CHECK UPDATES ###>
         if(.\update-profile.ps1){throw "Profile was updated, please rerun Profile load.";}
+
+    <### GET CREDENTIALS ###>
+    
+    # Get credentials
+    # Should this be in the beginning?
+    if($XMLReader.Machine.Secure.ToBoolean($null) -and !$LoggedIn)
+    {
+        $cred = Get-Content ($AppPointer.Machine.GitRepoDir + "\bin\credentials\user.JSON") | ConvertFrom-Json  
+        [string]$user = Read-Host -prompt "Username"; 
+
+        # Get Secure string and then convert it back to plain text
+        [System.Object]$var = Read-Host -prompt "Password" -AsSecureString; 
+        [System.ValueType]$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($var)
+        [String]$password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+        
+        if(($user -ne $cred.Username) -or ($cred.Password -ne $password))
+        {
+            Write-Error "WRONG CREDENTIALS";
+            Pop-Location;
+            exit;
+        }
+        else{[Boolean]$LoggedIn = $true;}
+    }
+
+    # Background setting for write-progress
+    Import-Module .\Modules\Terminal.psm1 -DisableNameChecking -Scope Local;
+    _SetBackgroundColor;
         
     <### PROGRAMS ###> 
         LoadPrograms -XMLReader:$XMLReader -Verbose:$Verbose

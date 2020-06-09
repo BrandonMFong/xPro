75b425ef59f68b94e58a73b1aebdaad72693f59c
Brandon Fong <brandon.fong@kiran.com>
2020-06-09 16:20:48 -0700

    [Code] reset string

--- Functions/DismountDrive.ps1
+++ Functions/DismountDrive.ps1
@@ -2,4 +2,4 @@ param([Parameter(Mandatory=$true)][string]$Drive)
 
 if(!(Test-Path $Drive)){Write-Warning "Drive $($Drive) does not exist.";  break;}
 $driveEject = New-Object -comObject Shell.Application;
-$driveEject.Namespace(17).ParseName("E:").InvokeVerb("Eject");
+$driveEject.Namespace(17).ParseName($Drive).InvokeVerb("Eject");

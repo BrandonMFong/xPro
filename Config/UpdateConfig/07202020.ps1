898c09c30467a2d58726329b4cadb78bcd76133f
BrandonMFong <fong.m.brandon97@gmail.com>
2020-07-20 18:12:14 -0700

    [Config Script] Updating the security to false

---
+++ Config/UpdateConfig/07202020.ps1
@@ -0,0 +1,34 @@
+<#
+.Synopsis
+   Setting Secure to FALSE
+   Must redo the credentials
+#>
+$error.Clear()
+try 
+{
+   Import-Module $($PSScriptRoot + "\..\..\Modules\FunctionModules.psm1") -Scope Local;
+   [System.Xml.XmlDocument]$xml = _GetXMLContent;
+   [String]$FilePath = _GetXMLFilePath;
+   
+   <# UPDATE START #>
+
+   if(![string]::IsNullOrEmpty($xml.Machine.ShellSettings.Security.Secure))
+   {
+      $xml.Machine.ShellSettings.Security.Secure.InnerXml = "False";
+   }
+
+   <# UPDATE END #>
+   
+   [System.XML.XMLElement]$UpdateStamp = $xml.Machine.UpdateStamp;
+   [string[]]$Check = (Get-ChildItem $PSScriptRoot\*.*).Name; # Only update scripts here
+   $UpdateStamp.SetAttribute("Value",$MyInvocation.MyCommand.Name.Replace('.ps1','')); # TODO put date
+   $UpdateStamp.SetAttribute("Count",$Check.Count); # TODO get ride of this
+   $xml.Save($FilePath);
+   return [int]0;
+}
+catch
+{
+   Write-Host "`nError in $($PSScriptRoot)\$($MyInvocation.MyCommand.Name) at line: $($_.InvocationInfo.ScriptLineNumber)" -ForegroundColor Red;
+   Write-Host "`n$($_.Exception)`n" -ForegroundColor Red;
+   return [int]1;
+}

c3ab7d6ade7753a0662025d836c21f19a235c8c6
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-13 20:38:13 -0700

    [Code] Work on yml file

---
+++ Build/Profile.ps1
@@ -0,0 +1,114 @@
+# Engineer: Brandon Fong
+# Important Global Variables: XmlReader, AppPointer, LogHandler
+Param([System.Boolean]$StartDir=$true,[System.Boolean]$StartScript=$true,[System.Boolean]$DebugFlag=$false)
+
+<### CONFIG ###>
+Push-Location $($PROFILE |Split-Path -Parent);
+    [System.Xml.XmlDocument]$AppPointer = Get-Content Profile.xml;
+Pop-Location
+[System.Xml.XmlDocument]$XMLReader = Get-Content $($AppPointer.Machine.GitRepoDir + "\Config\" + $AppPointer.Machine.ConfigFile);
+
+if(!$XMLReader.Machine.LoadProfile.ToBoolean($null)){break;} # Flag to load profile (in case someone wanting to use powershell)
+if($XMLReader.Machine.LoadProcedure -eq "Verbose"){[System.Boolean]$Verbose = $true} # Helps debugging if on
+else{[System.Boolean]$Verbose = $false}
+
+Push-Location $AppPointer.Machine.GitRepoDir; 
+    Import-Module .\Modules\FunctionModules.psm1 -DisableNameChecking:$true -Scope Local -WarningAction SilentlyContinue;
+
+    <### LOGS ###>
+        $global:LogHandler = Get-LogObject;
+
+    <### CHECK UPDATES ###>
+        if((.\update-profile.ps1) -or (.\update-config.ps1 -CheckUpdate))
+        {
+            if("y" -eq $(Read-Host -Prompt "Do you want to restart powershell? (y/n)"))
+            {
+                if($PSVersionTable.PSVersion.Major -lt 7){Start-Process powershell;Stop-Process -Id $PID;}
+                else{Start-Process pwsh;Stop-Process -Id $PID;}
+            }
+            else{exit;}
+        }
+
+    <### GET CREDENTIALS ###>
+        CheckCredentials;
+    
+    # Background setting for write-progress
+        Import-Module .\Modules\Terminal.psm1 -DisableNameChecking:$true -Scope Local -WarningAction SilentlyContinue;
+        _SetBackgroundColor;
+        
+    <### PROGRAMS ###> 
+        LoadPrograms -XMLReader:$XMLReader -Verbose:$Verbose
+    
+    <### MODULES ###>
+        LoadModules -XMLReader:$XMLReader -Verbose:$Verbose
+        
+    <### OBJECTS ###>
+        LoadObjects -XMLReader:$XMLReader -Verbose:$Verbose
+
+    <### NETWORK DRIVES ###>
+        LoadDrives -XMLReader:$XMLReader -Verbose:$Verbose
+
+    <### FUNCTIONS ###>
+        LoadFunctions -XMLReader:$XMLReader -Verbose:$Verbose
+        
+    <### START ###>
+        if(($XMLReader.Machine.Start.Enabled -eq "true") -and ($StartScript) -and ![string]::IsNullOrEmpty($XMLReader.Machine.Start)) 
+        {
+            if($XMLReader.Machine.Start.ClearHost -eq "true"){Clear-Host;} # clears host from the progess text
+
+            # Some output methods that should be defined
+            # Greetings, calendar
+
+            # Greetings
+            if(![string]::IsNullOrEmpty($XMLReader.Machine.Start.Greetings))
+            {
+                if([String]::IsNullOrEmpty($XMLReader.Machine.Start.Greetings.Type)){[String]$Type = "Big";}
+                else{[String]$Type = $XMLReader.Machine.Start.Greetings.Type;}
+                [System.Boolean]$Save = [System.Boolean]$XMLReader.Machine.Start.Greetings.Save;
+                $arg = 
+                @{
+                    string=$XMLReader.Machine.Start.Greetings.InnerXml; # String 
+                    Type=$Type; # Type of font
+                    SaveToFile=$Save;
+                };
+                [String]$GreetingsPath = (Get-ChildItem $(".\Functions\Greetings.ps1")).FullName; # Gets the full file path to the greetings script
+                & $GreetingsPath @arg;
+            }
+
+            # If the script is defined, run it
+            if(![String]::IsNullOrEmpty($XMLReader.Machine.Start.Script) -and $(Test-Path $XMLReader.Machine.Start.Script))
+            {
+                Invoke-Expression $(Evaluate -value:$XMLReader.Machine.Start.Script);# Executes the file that the user defines
+            } 
+        }
+    
+    try 
+    {
+        if(![String]::IsNullOrEmpty($XMLReader.Machine.ShellSettings.GitDisplay) -and ($XMLReader.Machine.ShellSettings.Enabled.ToBoolean($null)))
+        {
+            [String]$gitstring = "Version: $(git describe --tags)"
+            if($gitstring.Contains("-")){Write-Host "`n$($gitstring.Substring(0,$gitstring.IndexOf("-")))`n" -ForegroundColor Gray;}
+            else {Write-Host "`n$($gitstring)`n" -ForegroundColor Gray;}
+        }
+    }
+    catch 
+    {
+        $LogHandler.Write("You may not have git bash installed.")
+    }
+
+Pop-Location;
+
+# Method for start directory 
+if($StartDir -and (![String]::IsNullOrEmpty($XMLReader.Machine.ShellSettings.StartDirectory)))
+{Set-Location $XMLReader.Machine.ShellSettings.StartDirectory;}
+
+# For debug mode
+# It will run the debug script after profile is loaded
+if($DebugFlag)
+{
+    $DebugScript = $($AppPointer.Machine.GitRepoDir + "\Resources\Debug.ps1");
+    if(!(Test-Path $DebugScript)){New-Item $DebugScript;}
+    & $DebugScript;
+}
+
+$global:LogHandler.Write("Successfully loaded profile");

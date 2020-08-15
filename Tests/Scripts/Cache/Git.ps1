006e0a9242e7a4bc0b16e657f3aceb0a83be8106
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-14 23:31:59 -0700

    [SQUASH] tests-dev-Replacements => dev

--- Tests/Scripts/Cache/Git.ps1
+++ Tests/Scripts/Cache/Git.ps1
@@ -1,12 +1,25 @@
 <#
 .Synopsis
-   Checking cache records
+   In order for the Terminal replacements to be efficient, I cache the file path in a file
 #>
 Write-Host `n -NoNewline;
 [Byte]$RETURNVALUE = 0;
 <# TEST START #>
 
-Write-Host " - TODO describe test" -NoNewline;
+Write-Host " - Git cache file created" -NoNewline;
+
+# @gitbranch
+[string]$string="@gitbranch";
+_Replace([ref]$string); # Makes the cache file
+# [string]$CheckString = 'D.a.XmlPSProfile.XmlPSProfile.Tests';
+[string]$CheckString = $($Global:AppPointer.Machine.GitRepoDir + "\Tests").Replace("\",".").Replace(":",".").Replace("..",".");
+if(!(Test-Path $($Global:AppPointer.Machine.GitRepoDir + "\Cache\git\" + $CheckString)))
+{
+    Write-Host " [FAILED]" -ForegroundColor Red;
+    $RETURNVALUE = 1;
+}
+else{Write-Host " [PASSED]" -ForegroundColor Green;}
+
 
 <# TEST END #>
 return $RETURNVALUE;
--- Tests/Scripts/Terminal/Replacements.ps1
+++ Tests/Scripts/Terminal/Replacements.ps1
@@ -1,6 +1,6 @@
 <#
 .Synopsis
-   Checking if Greetings file was created if greetings is configured
+   Running through replacements for tags
 #>
 Write-Host `n -NoNewline;
 [Byte]$RETURNVALUE = 0;
@@ -8,15 +8,17 @@ Write-Host `n -NoNewline;
 Import-Module $($PSScriptRoot + "\..\..\..\Modules\Terminal.psm1") -Scope Local;
 
 Write-Host " - Checking replacements";
+
+# @fulldir
 Write-Host "   - Tag @fulldir" -NoNewline;
 [string]$string="@fulldir";
 _Replace([ref]$string)
 if($PSScriptRoot -eq "D:\a\XmlPSProfile\XmlPSProfile\Tests\Scripts\Terminal") # On VM
 {
     [string]$CheckString = 'D:\a\XmlPSProfile\XmlPSProfile\Tests';
-    if($string -eq $CheckString)
+    if($string -ne $CheckString)
     {
-       Write-Host " [FAILED]" -ForegroundColor Red;
+       Write-Host " != $($string) [FAILED]" -ForegroundColor Red;
        $RETURNVALUE = 1;
     }
     else{Write-Host " [PASSED]" -ForegroundColor Green;}
@@ -26,15 +28,17 @@ else
     Write-Host " [WARNING]" -ForegroundColor DarkYellow;
     Write-Host "    - Not on VM, @fulldir = $($string)" -ForegroundColor Gray;
 }
+
+# @currdir
 Write-Host "   - Tag @currdir" -NoNewline;
 [string]$string="@currdir";
 _Replace([ref]$string)
 if($PSScriptRoot -eq "D:\a\XmlPSProfile\XmlPSProfile\Tests\Scripts\Terminal") # On VM
 {
     [string]$CheckString = 'Tests';
-    if($string -eq $CheckString)
+    if($string -ne $CheckString)
     {
-       Write-Host " [FAILED]" -ForegroundColor Red;
+       Write-Host " != $($string) [FAILED]" -ForegroundColor Red;
        $RETURNVALUE = 1;
     }
     else{Write-Host " [PASSED]" -ForegroundColor Green;}
@@ -44,6 +48,18 @@ else
     Write-Host " [WARNING]" -ForegroundColor DarkYellow;
     Write-Host "    - Not on VM, @currdir = $($string)" -ForegroundColor Gray;
 }
+
+# @date
+Write-Host "   - Tag @date" -NoNewline;
+[string]$string="@date";
+_Replace([ref]$string)
+[string]$CheckString = Get-Date -Format $Global:XMLReader.Machine.ShellSettings.Format.Date;
+if($string -ne $CheckString)
+{
+    Write-Host " != $($string) [FAILED]" -ForegroundColor Red;
+    $RETURNVALUE = 1;
+}
+else{Write-Host " [PASSED]" -ForegroundColor Green;}
 
 
 <# TEST END #>

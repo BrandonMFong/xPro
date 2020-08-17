b47dc95f68bd1709a54ce0e5d9ef48591c9f5da8
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-17 14:48:11 -0700

    [Code] Updating schema path

---
+++ Config/UpdateConfig/08172020.ps1
@@ -0,0 +1,28 @@
+<#
+.Synopsis
+   Updating Schema path
+#>
+Param([ref]$Executed)
+$error.Clear();
+try 
+{
+   Import-Module $($PSScriptRoot + "\..\..\Modules\FunctionModules.psm1") -Scope Local;
+   [System.Xml.XmlDocument]$xml = _GetXMLContent;
+   [String]$FilePath = _GetXMLFilePath;
+   
+   <# UPDATE START #>
+
+   $xml.Machine.noNamespaceSchemaLocation = "..\..\Schema\Powershell.xsd";
+
+   <# UPDATE END #>
+   
+   [System.XML.XMLElement]$UpdateStamp = $xml.Machine.UpdateStamp;
+   $UpdateStamp.SetAttribute("Value",$MyInvocation.MyCommand.Name.Replace('.ps1','')); # TODO put date
+   $xml.Save($FilePath);
+   $Executed.Value = $true;
+}
+catch
+{
+   $Global:LogHandler.WriteError($_);
+   $Executed.Value = $false;
+}

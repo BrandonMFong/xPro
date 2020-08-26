2660a545928aec50dc3330721afacc3e82cb418e
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-26 09:59:38 -0700

    Not using this anymore, just straight up copy the config

--- Functions/Include-Sql-Config.ps1
+++
@@ -1,6 +0,0 @@
-<#
-.Synopsis
-    If user wants to include the SQL config, they can run this script to include it into their config file 
-#>
-
-[System.Xml.XmlDocument]$BaseConfig = Get-Content $($Global:AppPointer.Machine.GitRepoDir + $AppJson)

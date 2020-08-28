1e19e1c2dcbb517ab4a4187fe73412b241b52605
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-27 21:32:58 -0700

    [SQUASH] update-dev-AllowChoiceForNetworkSharedDrives => dev
    * [2448001 - Thu Aug 27 21:32:44 2020 -0700] [Code] Show drive letter
    |
    * [3c18fd3 - Thu Aug 27 21:31:45 2020 -0700] [Code] Rearranging param
    |
    * [07e71ca - Thu Aug 27 21:29:48 2020 -0700] [Code] Clean up former verbose option and showing connection to user
    |
    * [df87ec8 - Thu Aug 27 21:26:18 2020 -0700] [Code] Setting check for all
    |
    * [6d314c9 - Thu Aug 27 21:23:27 2020 -0700] [Code] Adding id param
    |
    * [bb3287b - Thu Aug 27 21:21:57 2020 -0700] [Config] Adding id
    |
    * [2ed23dd - Thu Aug 27 21:19:56 2020 -0700] [Code] Update script to add ID
    |
    * [d3305a4 - Thu Aug 27 21:18:11 2020 -0700] [Code] Removing unwanted comments

---
+++ Config/UpdateConfig/08272020.ps1
@@ -0,0 +1,41 @@
+<#
+.Synopsis
+   Adding ID to Network share type
+#>
+Param([ref]$Executed)
+$error.Clear();
+try 
+{
+   Import-Module $($PSScriptRoot + "\..\..\Modules\FunctionModules.psm1") -Scope Local;
+   [System.Xml.XmlDocument]$xml = _GetXMLContent;
+   [String]$FilePath = _GetXMLFilePath;
+   
+   <# UPDATE START #>
+
+   if(![string]::IsNullOrEmpty($xml.Machine.Networks)) 
+   {
+      foreach($Network in $xml.Machine.Networks.Network)
+      {
+         foreach($Connection in $Network.Connection)
+         {
+            if($Connection.Type -eq "NetworkShare")
+            {
+               [System.Xml.XmlElement]$ID = $xml.CreateElement("ID");
+               $Connection.InsertBefore($ID,$Connection.IPAddress);
+            }
+         }
+      }
+   }
+
+   <# UPDATE END #>
+   
+   [System.XML.XMLElement]$UpdateStamp = $xml.Machine.UpdateStamp;
+   $UpdateStamp.SetAttribute("Value",$MyInvocation.MyCommand.Name.Replace('.ps1','')); # TODO put date
+   $xml.Save($FilePath);
+   $Executed.Value = $true;
+}
+catch
+{
+   $Global:LogHandler.WriteError($_);
+   $Executed.Value = $false;
+}

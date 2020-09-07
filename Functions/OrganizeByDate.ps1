f91432718d2452e1e17be13a68656cfc928b2225
BrandonMFong <fong.m.brandon97@gmail.com>
2020-09-07 10:20:20 -0700

    [Code] Adding key detection

--- Functions/OrganizeByDate.ps1
+++ Functions/OrganizeByDate.ps1
@@ -9,15 +9,18 @@ Param(
     [String]$DestinationDirectory = "K:\PHOTOS\",
     [String]$DirectoryNameFormatter = "yyyy-MM"
 )
+
+Import-Module $($PSScriptRoot + "\..\Modules\FunctionModules.psm1") -Scope Local;
+
 [DateTime]$StartTime = Get-Date;
 # Set file paths
 [String]$DestinationPath = $null;
 
 [int16]$NumberOfFiles = (Get-ChildItem $SourcePath).Length;
+if($NumberOfFiles -eq 0){throw "No files!";}
 
 # Get all the unique months
 [System.Object]$YoungestFile = Get-ChildItem $SourcePath | Sort-Object -Property LastWriteTime | Select-Object -First 1;
-# [System.Object]$OldestFile = Get-ChildItem $SourcePath | Sort-Object -Property LastWriteTime | Select-Object -Last 1;
 
 # Init definition
 # Defines the interval of the how the files will go into a directory 
@@ -25,7 +28,8 @@ Param(
 [DateTime]$maxdate= $YoungestFile.LastWriteTime.ToDateTime($null).AddMonths(1);
 try 
 {
-    while($true)
+    [bool]$key = $false;
+    while(!$key)
     {
         # If there are no more files left in the directory 
         if($(Get-ChildItem $SourcePath).Length -eq 0){break;}
@@ -34,10 +38,14 @@ try
         if(!(Test-Path $DestinationPath)){mkdir $DestinationPath;} # make the directory if it does not exist
     
         # Move the files
-        Get-ChildItem $SourcePath | Where-Object{($_.LastWriteTime -gt $mindate) -and ($_.LastWriteTime -lt $maxdate)}|ForEach-Object{Move-Item $_ $DestinationPath -Verbose -Force}
+        Get-ChildItem $SourcePath | Where-Object{($_.LastWriteTime -ge $mindate) -and ($_.LastWriteTime -lt $maxdate)}|ForEach-Object{Move-Item $_.FullName $DestinationPath -Verbose -Force}
     
         # increment the interval
         $mindate=$mindate.AddMonths(1);$maxdate=$maxdate.AddMonths(1);
+
+        Write-Host "Next Interval (Press 'Q' to quit): mindate=$($mindate) & maxdate=$($maxdate)";
+        Start-Sleep 5; # Giving me time to stop the transfer
+        $key = Test-KeyPress -Key Q; # test key
     }
 }
 catch 
--- Modules/FunctionModules.psm1
+++ Modules/FunctionModules.psm1
@@ -639,3 +639,31 @@ function GetSynopsisText
         if(($Content[$i].Substring(0,$Content[$i].Length) -eq ".Synopsis") -and !$FoundSynopsis){$FoundSynopsis = $true;}
     }
 }
+
+function Test-KeyPress
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [ConsoleKey]
+        $Key,
+
+        [System.ConsoleModifiers]
+        $ModifierKey = 0
+    )
+    if ([Console]::KeyAvailable)
+    {
+        $pressedKey = [Console]::ReadKey($true)
+
+        $isPressedKey = $key -eq $pressedKey.Key
+        if ($isPressedKey)
+        {
+            return ($pressedKey.Modifiers -eq $ModifierKey);
+        }
+        else
+        {
+            return $false
+        }
+    }
+}
+

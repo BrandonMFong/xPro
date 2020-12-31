4ff87e8056c27039da5f6cb50650f226797974dc
BrandonMFong <fong.m.brandon97@gmail.com>
2020-12-30 21:25:34 -0800

    successfully listed directory items

--- Library/PrintConfigFiles/main.cpp
+++ Library/PrintConfigFiles/main.cpp
@@ -4,13 +4,22 @@
 #include <iostream>
 #include <string>
 #include <experimental/filesystem>
+
+#ifdef _WIN32
+#define PathSeparator '\\'
+#elif __linux__ 
+#define PathSeparator '/'
+#endif
+
 namespace fs = std::experimental::filesystem::v1;
 
 int main(int argc, char *argv[]) 
 {
-    // Since there should only be two, this is the index to get the argument I want
-    int index = 1; 
-    std::string path;
+    int index = 1; // we are only accepting one argument here
+    std::string path; // will hold the argument 
+    std::string filepath; // will hold each file path in the directory pointed to by the argument 
+    char sep = PathSeparator; // defines how the file paths are separated
+    int count = 1;
 
     // Exit program if this is met
     if (argc != 2)
@@ -19,14 +28,22 @@ int main(int argc, char *argv[])
         return 1; // should be less than or equal to two 
     } 
 
+    // Since there should only be two, this is the index to get the argument I want
     path = argv[index];
 
-    std::string filepath;
     for (const auto & entry : fs::directory_iterator(path)) 
     {
-        filepath = entry.path();
-        std::cout << filepath << std::endl;
+        filepath = entry.path(); // apply to string 
+        size_t i = filepath.rfind(sep, filepath.length()); // find the positions of the path delimiters
+        
+        // if no failure
+        if (i != std::string::npos)  filepath = filepath.substr(i+1, filepath.length() - i);
+        
+        // Print out items
+        std::cout << "[" << count << "] " << filepath << std::endl;
+
+        count++;
     }
-  
+
     return 0;
 }

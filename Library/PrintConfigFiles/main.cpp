81eabb37bb3b71c130c98401450b38e81b13ef62
BrandonMFong <fong.m.brandon97@gmail.com>
2020-12-30 21:11:09 -0800

    printed full path

--- Library/PrintConfigFiles/main.cpp
+++ Library/PrintConfigFiles/main.cpp
@@ -1,4 +1,5 @@
 // xPro
+// g++ main.cpp -lstdc++fs -
 
 #include <iostream>
 #include <string>
@@ -9,16 +10,23 @@ int main(int argc, char *argv[])
 {
     // Since there should only be two, this is the index to get the argument I want
     int index = 1; 
-    std::string path = argv[index];
+    std::string path;
 
     // Exit program if this is met
-    if (argc > 2)
+    if (argc != 2)
     {
-        std::cout << "Too many arguments" << std::endl;
+        std::cout << "No argument was passed" << std::endl;
         return 1; // should be less than or equal to two 
     } 
 
-    for (const auto & entry : fs::directory_iterator(path)) std::cout << entry.path() << std::endl;
+    path = argv[index];
+
+    std::string filepath;
+    for (const auto & entry : fs::directory_iterator(path)) 
+    {
+        filepath = entry.path();
+        std::cout << filepath << std::endl;
+    }
   
     return 0;
 }

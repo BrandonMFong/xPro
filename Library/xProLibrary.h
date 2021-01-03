4baeff0a570f2af158a187b52cdce19a54b0e362
BrandonMFong <fong.m.brandon97@gmail.com>
2021-01-03 14:43:18 -0800

    [Code] compile function from lib

--- Library/xProLibrary.h
+++ Library/xProLibrary.h
@@ -27,4 +27,35 @@ namespace fs = std::experimental::filesystem;
 namespace fs = std::__fs::filesystem;
 #endif
 
+// Functions 
+void enumItemsInDir(std::string path)
+{
+    std::string filename;
+    int count = 1;
+
+    // If this isn't windows, define these variables for the operations of getting the file
+    #if !defined(_WIN32) || !defined(_WIN64)
+    std::string filepath; // will hold each file path in the directory pointed to by the argument 
+    char sep = PathSeparator; // defines how the file paths are separated
+    #endif
+
+    for (const auto & entry : fs::directory_iterator(path)) 
+    {
+        #if defined(_WIN32) || defined(_WIN64)
+        filename = entry.path().filename().string(); // apply to string 
+        #else 
+        filepath = entry.path(); // apply to string 
+        size_t i = filepath.rfind(sep, filepath.length()); // find the positions of the path delimiters
+        
+        // if no failure
+        if (i != std::string::npos)  filename = filepath.substr(i+1, filepath.length() - i);
+        #endif
+        
+        // Print out items
+        std::cout << "[" << count << "] " << filename << std::endl;
+
+        count++;
+    }
+}
+
 #endif

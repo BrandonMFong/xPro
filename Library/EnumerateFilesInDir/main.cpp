ded73c5fe281dd2c7619b40e81edb1245f5099ff
BrandonMFong <fong.m.brandon97@gmail.com>
2021-01-03 14:43:35 -0800

    [Code] tidy up

--- Library/EnumerateFilesInDir/main.cpp
+++ Library/EnumerateFilesInDir/main.cpp
@@ -7,10 +7,6 @@ int main(int argc, char *argv[])
 {
     int index = 1; // we are only accepting one argument here
     std::string path; // will hold the argument 
-    // std::string filepath; // will hold each file path in the directory pointed to by the argument 
-    // std::string filename;
-    // char sep = PathSeparator; // defines how the file paths are separated
-    // int count = 1;
 
     // Exit program if this is met
     if (argc != 2)
@@ -22,26 +18,7 @@ int main(int argc, char *argv[])
     // Since there should only be two, this is the index to get the argument I want
     path = argv[index];
 
-    // for (const auto & entry : fs::directory_iterator(path)) 
-    // {
-    //     #if defined(_WIN32) || defined(_WIN64)
-    //     filename = entry.path().filename().string(); // apply to string 
-    //     // std::cout << filepath << std::endl;
-    //     #else 
-    //     filepath = entry.path(); // apply to string 
-    //     size_t i = filepath.rfind(sep, filepath.length()); // find the positions of the path delimiters
-        
-    //     // if no failure
-    //     if (i != std::string::npos)  filename = filepath.substr(i+1, filepath.length() - i);
-    //     #endif
-        
-    //     // Print out items
-    //     std::cout << "[" << count << "] " << filename << std::endl;
-
-    //     count++;
-    // }
     enumItemsInDir(path);
-
 
     return 0;
 }

05084c5f6c22b004de80d0ce1700e04e882d7048
BrandonMFong <fong.m.brandon97@gmail.com>
2021-01-19 20:51:19 -0800

    using correctly xml_node object

--- Sources/xPro/xConfigReader.cpp
+++ Sources/xPro/xConfigReader.cpp
@@ -18,143 +18,146 @@ xConfigReader::xConfigReader(xString filepath) : xXml(xDefaultConfigRootNodeName
     rapidxml::xml_node<>    * root, 
                             * child0,
                             * child1,
-                            * child2;
+                            * child2,
+                            * child3;
     
     if(!okayToContinue)  return; 
 
     root = this->_xmlDocument.first_node(this->_rootNodeName.c_str()); // Get the root node's xmlnode object
-    child0 = root->first_node(); // init with first node of the root node's children 
+    child0 = root->first_node("Machine"); // init with first node of the root node's children 
 
+    std::cout << "Test" << std::endl;
     this->Machine.MachineName = child0->first_attribute("MachineName")->value(); 
     this->Machine.LoadProcedure = child0->first_attribute("LoadProcedure")->value(); 
     this->Machine.LoadProcedure = child0->first_attribute("LoadProfile")->value();
 
-    while(child0)
+    child1 = child0->first_node("UpdateStamp");
+    while(child1)
     {
         // UpdateStamp
-        if(strcmp(child0->name(),"UpdateStamp") == 0) this->Machine.UpdateStamp.Value = child1->first_attribute("Value")->value(); 
+        if(strcmp(child1->name(),"UpdateStamp") == 0) this->Machine.UpdateStamp.Value = child1->first_attribute("Value")->value(); 
 
         // ShellSettings
-        else if(strcmp(child0->name(),"ShellSettings") == 0)
+        else if(strcmp(child1->name(),"ShellSettings") == 0)
         {
-            child1 = child0->first_node();
+            child2 = child1->first_node();
 
-            this->Machine.ShellSettings.Enabled = child1->first_attribute("Enabled")->value();
+            this->Machine.ShellSettings.Enabled = child2->first_attribute("Enabled")->value();
 
-            while(child1)
+            while(child2)
             {
-                if(strcmp(child1->name(),"Prompt") == 0)
+                if(strcmp(child2->name(),"Prompt") == 0)
                 {
-                    child2 = child1->first_node();
+                    child3 = child2->first_node();
 
-                    this->Machine.ShellSettings.Prompt.Enabled = child2->first_attribute("Enabled")->value();
+                    this->Machine.ShellSettings.Prompt.Enabled = child3->first_attribute("Enabled")->value();
 
-                    while(child2)
+                    while(child3)
                     {
-                        if(strcmp(child2->name(),"BaterryLifeThreshold") == 0)
+                        if(strcmp(child3->name(),"BaterryLifeThreshold") == 0)
                         {
-                            this->Machine.ShellSettings.Prompt.BaterryLifeThreshold.Enabled = child2->first_attribute("Enabled")->value();
-                            this->Machine.ShellSettings.Prompt.BaterryLifeThreshold.InnerXml = child2->value();
+                            this->Machine.ShellSettings.Prompt.BaterryLifeThreshold.Enabled = child3->first_attribute("Enabled")->value();
+                            this->Machine.ShellSettings.Prompt.BaterryLifeThreshold.InnerXml = child3->value();
                         }
-                        else if(strcmp(child2->name(),"String") == 0)
+                        else if(strcmp(child3->name(),"String") == 0)
                         {
-                            this->Machine.ShellSettings.Prompt.String.Color = child2->first_attribute("Color")->value();
-                            this->Machine.ShellSettings.Prompt.String.InnerXml = child2->value();
+                            this->Machine.ShellSettings.Prompt.String.Color = child3->first_attribute("Color")->value();
+                            this->Machine.ShellSettings.Prompt.String.InnerXml = child3->value();
                         }
                     }
                 }
-                else if(strcmp(child1->name(),"StartDirectory") == 0) this->Machine.ShellSettings.StartDirectory.InnerXml = child1->value();
+                else if(strcmp(child2->name(),"StartDirectory") == 0) this->Machine.ShellSettings.StartDirectory.InnerXml = child2->value();
 
-                child1 = child1->next_sibling();
+                child2 = child2->next_sibling();
             }
         }
 
         // Modules
-        else if(strcmp(child0->name(),"Modules") == 0)
+        else if(strcmp(child1->name(),"Modules") == 0)
         {
-            child1 = child0->first_node();
+            child2 = child1->first_node();
 
             Root::Modules::Mod * tempMod;
-            while(child1)
+            while(child2)
             {
                 tempMod = new Root::Modules::Mod(); // new object
-                tempMod->InnerXml = child1->value(); // Get the value
+                tempMod->InnerXml = child2->value(); // Get the value
                 this->Machine.Modules.Module.push_back(*tempMod); // insert into array 
 
-                child1 = child1->next_sibling();
+                child2 = child2->next_sibling();
             }
         }
 
         // Directories
-        else if(strcmp(child0->name(),"Directories") == 0)
+        else if(strcmp(child1->name(),"Directories") == 0)
         {
-            child1 = child0->first_node();
+            child2 = child1->first_node();
 
             Root::Directories::Dir * tempDir;
-            while(child1)
+            while(child2)
             {
                 tempDir = new Root::Directories::Dir; // new object
-                tempDir->Alias = child1->first_attribute("Alias")->value();
-                tempDir->SecType = child1->first_attribute("SecType")->value();
-                tempDir->InnerXml = child1->value(); // Get the value
+                tempDir->Alias = child2->first_attribute("Alias")->value();
+                tempDir->SecType = child2->first_attribute("SecType")->value();
+                tempDir->InnerXml = child2->value(); // Get the value
                 this->Machine.Directories.Directory.push_back(*tempDir); // insert into array 
 
-                child1 = child1->next_sibling();
+                child2 = child2->next_sibling();
             }
         }
 
         // Programs
-        else if(strcmp(child0->name(),"Programs") == 0)
+        else if(strcmp(child1->name(),"Programs") == 0)
         {
-            child1 = child0->first_node();
+            child2 = child1->first_node();
 
             Root::Programs::Prog * tempProg;
-            while(child1)
+            while(child2)
             {
                 tempProg = new Root::Programs::Prog; // new object
-                tempProg->Alias = child1->first_attribute("Alias")->value();
-                tempProg->SecType = child1->first_attribute("SecType")->value();
-                tempProg->InnerXml = child1->value(); // Get the value
+                tempProg->Alias = child2->first_attribute("Alias")->value();
+                tempProg->SecType = child2->first_attribute("SecType")->value();
+                tempProg->InnerXml = child2->value(); // Get the value
                 this->Machine.Programs.Program.push_back(*tempProg); // insert into array 
 
-                child1 = child1->next_sibling();
+                child2 = child2->next_sibling();
             }
         }
 
-        else if(strcmp(child0->name(),"Objects") == 0)
+        else if(strcmp(child1->name(),"Objects") == 0)
         {
-            child1 = child0->first_node();
+            child2 = child1->first_node();
 
             Root::Objects::Obj * tempObj;
-            while(child1)
+            while(child2)
             {
-                child2 = child1->first_node();
+                child3 = child2->first_node();
 
                 tempObj = new Root::Objects::Obj;
-                tempObj->Type = child1->first_attribute("Type")->value();
+                tempObj->Type = child2->first_attribute("Type")->value();
                 
-                while(child2)
+                while(child3)
                 {
-                    if(strcmp(child2->name(),"VarName") == 0)
+                    if(strcmp(child3->name(),"VarName") == 0)
                     {
-                        tempObj->VarName.SecType = child2->first_attribute("SecType")->value();
-                        tempObj->VarName.InnerXml = child2->value();
+                        tempObj->VarName.SecType = child3->first_attribute("SecType")->value();
+                        tempObj->VarName.InnerXml = child3->value();
                     }
-                    else if(strcmp(child2->name(),"SimpleValue") == 0)
+                    else if(strcmp(child3->name(),"SimpleValue") == 0)
                     {
-                        tempObj->SimpleValue.SecType = child2->first_attribute("SecType")->value();
-                        tempObj->SimpleValue.InnerXml = child2->value();
+                        tempObj->SimpleValue.SecType = child3->first_attribute("SecType")->value();
+                        tempObj->SimpleValue.InnerXml = child3->value();
                     }
 
-                    child2 = child2->next_sibling();
+                    child3 = child3->next_sibling();
                 }
 
                 this->Machine.Objects.Object.push_back(*tempObj);
 
-                child1 = child1->next_sibling();
+                child2 = child2->next_sibling();
             }
         }
 
-        child0 = child0->next_sibling();
+        child1 = child1->next_sibling();
     }
 }

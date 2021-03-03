28447285f41d24e25da34123ea7edaa30a7b340a
BrandonMFong <fong.m.brandon97@gmail.com>
2021-03-02 21:40:02 -0800

    Putting Update config logic to setup script

--- setup.ps1
+++ setup.ps1
@@ -69,6 +69,71 @@ function UpdateProfile
    return $status;
 }
 
+function UpdateConfig()
+{
+   Param([string]$ConfigName=$null,[Switch]$CheckUpdate)
+   Push-Location $PSScriptRoot
+   [System.Object[]]$AppJson = Get-Content .\Config\app.json|ConvertFrom-Json; # Get app config
+
+   # This segment runs independtly.  I could have made another script but the context of this script is similar to this segment's goal
+   # Can probably be arranged better
+   if($CheckUpdate)
+   {
+      [string[]]$Scripts = (Get-ChildItem $PSScriptRoot\Config\UpdateConfig\*.*).Name;
+
+      [String]$ParseString="MMddyyyy"
+      [String[]]$UpgradeScripts = (Get-ChildItem $PSScriptRoot\Config\UpdateConfig\*.*).BaseName; # Only using the base name to determine update stamp
+      $arg = @{Object=$UpgradeScripts;Method="SelectionSort";ParseString=$ParseString};
+      $command = $(Get-ChildItem $($PSScriptRoot + "\Scripts\Sort-Object.ps1")).FullName;
+      [string[]]$InOrderScripts = $(& $command @arg);
+
+      # There has to be a better way at checking if there is an update needed
+      if([DateTime]::ParseExact($Scripts[$Scripts.Count-1].Replace(".ps1",$null),$ParseString,$null) -gt [DateTime]::ParseExact($Global:XMLReader.Machine.UpdateStamp.Value,$ParseString,$null))
+      {
+         Write-Host  "`nThere is an update to GlobalScripts Config." -ForegroundColor Red
+         [string]$update = Read-Host -Prompt "Want to update? (y/n)";
+         if($update -eq "y")
+         {
+            Import-Module $($PSScriptRoot + "\Modules\ConfigHandler.psm1") -Scope Local -DisableNameChecking;
+            Run-Update -InOrderScripts:$InOrderScripts; # Updates configuration file
+            Pop-location; 
+
+            Write-Warning "Config was updated";
+            return 1; # Exiting code
+         }
+         else
+         {
+            Pop-Location; 
+            return 0;
+         }
+      }
+      else
+      {
+         Pop-location;
+         return 0;
+      }
+   }
+
+   # Create AppPointer
+   Write-Host "Config files to choose from:"
+   [string]$pathToxProConfig = "." + $AppJson.Directories.UserConfig
+   .\bin\xpro.enumdir.exe $pathToxProConfig; # Get items from the user config
+   $choice = Read-Host -Prompt "So"; # Get user's choice 
+   [String]$ConfigFile = $(.\bin\xpro.selectitem.exe -path $pathToxProConfig -index $($choice - 1)); # Get the index
+
+   Write-Host  "`nConfig => $($ConfigFile)`n" -ForegroundColor Cyan;
+
+   # write into apppointer
+   Push-Location $HOME;
+   # Push-Location $($PROFILE |Split-Path -Parent);
+   [System.Xml.XmlDocument]$XmlEditor = Get-Content .\Profile.xml; # read profile.xml
+   [String]$Path = $(Get-ChildItem .\Profile.xml).FullName; # get the path to the profile.xml
+   $XmlEditor.Machine.ConfigFile = "\" + $ConfigFile; # write configfile to AppPointer
+   $XmlEditor.Save($Path); # save 
+   Pop-Location
+   Pop-Location
+}
+
 if($status)
 {
    Write-Host "";
@@ -171,11 +236,12 @@ if($status)
 
    $tempContent = $(Get-Content $FileName);
 
+   # Useing contains because of the `n 
    $status = $FirstLine.Contains($tempContent[0]);
 
    if(!$status)
    {
-      Write-Warning "Firstline ($($FirstLine))was not written";
+      Write-Warning "Firstline was not written";
    }
 }
 
@@ -183,6 +249,7 @@ if($status)
 {
    [byte]$userIntInput = Read-Host -Prompt "What do you want to do?`nCreate New Config[1]`nUse Existing Confg[2]`nSo"
 
+   # New config
    if ($userIntInput -eq 1)
    {
       [String]$ConfigurationName = Read-Host -Prompt "Name the configuration file";
@@ -211,7 +278,8 @@ if($status)
    }
    elseif($userIntInput -eq 2) 
    {
-      .\.\update-config.ps1;
+      # .\.\update-config.ps1;
+      UpdateConfig;
    }
    else
    {

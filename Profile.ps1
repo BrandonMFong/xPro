d28a7c8004b315ec946222d706885c2c4fbd6c17
BrandonMFong <fong.m.brandon97@gmail.com>
2021-03-07 20:41:22 -0800

    Working on profile script

--- Profile.ps1
+++ Profile.ps1
@@ -8,112 +8,146 @@ Param(
     [System.Boolean]$Silent=$false # There is Verbose/Progress. Using Progress in github workflow looks silent
 )
 
+[System.Xml.XmlDocument]$Global:XMLReader;
+[System.Xml.XmlDocument]$Global:AppPointer;
+[String]$profileFilePath = "";
+[String]$GitRepoDir = "";
+[String]$AppJsonPath = "";
+[System.Object[]]$Global:AppJson;
+[String]$pathToConfig;
+[System.Boolean]$Verbose;
 try 
 {
     <### GET AppPointer ###>
-    if(![string]::IsNullOrEmpty($BuildPath)){Push-Location $($BuildPath|Split-Path -Parent);}
-    else{Push-Location $($PROFILE |Split-Path -Parent);}
-        [string]$profileFilePath = $HOME + "\\Profile.xml"; # TODO change 
-        [System.Xml.XmlDocument]$Global:AppPointer = Get-Content $profileFilePath; # Will always be Profile.xml
-    Pop-Location;
+    # For the CI test build
+    if(![String]::IsNullOrEmpty($BuildPath))
+    {
+        # Push-Location $($BuildPath | Split-Path -Parent);
+        $profileFilePath = "D:\a\xPro\xPro\" + $BuildPath
+    }
+    else
+    {
+        # Push-Location $($PROFILE |Split-Path -Parent);
+        $profileFilePath = $HOME + "\\Profile.xml"
+    }
+    
+    # [string]$profileFilePath = $HOME + "\\Profile.xml"; # TODO change 
+    $Global:AppPointer = [System.Xml.XmlDocument]$(Get-Content $profileFilePath); # Will always be Profile.xml
+
+    # Get Git repo dir
+    $GitRepoDir = $Global:AppPointer.Machine.GitRepoDir;
+    # Pop-Location;
     <### LOAD ###>
-    Push-Location $Global:AppPointer.Machine.GitRepoDir; 
-        <# GET App Json, think of a better name for this #>
-        [System.Object[]]$Global:AppJson = Get-Content .\Config\app.json|ConvertFrom-Json;
-        # pwsh
-        # Todo test different environments
-        [string]$pathToConfig = $Global:AppPointer.Machine.GitRepoDir + $Global:AppJson.Directories.UserConfig + $Global:AppPointer.Machine.ConfigFile;
-        if($IsWindows){[System.Xml.XmlDocument]$Global:XMLReader = Get-Content $pathToConfig;}
-        else{[System.Xml.XmlDocument]$Global:XMLReader = Get-Content $pathToConfig;}
-    
-        if(!$Global:XMLReader.Machine.LoadProfile.ToBoolean($null)){break;} # Flag to load profile (in case someone wanting to use powershell)
-        if(($Global:XMLReader.Machine.LoadProcedure -eq "Verbose") -and !$Silent){[System.Boolean]$Verbose = $true} # Helps debugging if on
-        else{[System.Boolean]$Verbose = $false}
-    
-        Import-Module .\Modules\xProUtilities.psm1 -DisableNameChecking:$true -Scope Local -WarningAction SilentlyContinue;
-    
-        <### LOGS ###>
-            $Global:LogHandler = Get-LogObject;
-    
-        <### CHECK UPDATES ###>
-            if([string]::IsNullOrEmpty($BuildPath))
-            {
-                if((.\update-profile.ps1) -or (.\update-config.ps1 -CheckUpdate))
-                {
-                    if("y" -eq $(Read-Host -Prompt "Do you want to restart powershell? (y/n)"))
-                    {
-                        if($PSVersionTable.PSVersion.Major -lt 7){Start-Process powershell;Stop-Process -Id $PID;}
-                        else{Start-Process pwsh;Stop-Process -Id $PID;}
-                    }
-                    else{exit;}
-                }
-            }
-    
-        <### GET CREDENTIALS ###>
-            CheckCredentials;
-        
-        <# BACKGROUND SETTINGS #>
-            # Doing this early so if Verbose:$false then progress color will be set
-            Import-Module .\Modules\Terminal.psm1 -DisableNameChecking:$true -Scope Local -WarningAction SilentlyContinue;
-            _SetBackgroundColor;
-            
-        <### PROGRAMS ###> 
-            LoadPrograms -XMLReader:$Global:XMLReader -Verbose:$Verbose
-        
-        <### MODULES ###>
-            LoadModules -XMLReader:$Global:XMLReader -Verbose:$Verbose
-            
-        <### OBJECTS ###>
-            LoadObjects -XMLReader:$Global:XMLReader -Verbose:$Verbose
-    
-        <### FUNCTIONS ###>
-            LoadFunctions -XMLReader:$Global:XMLReader -Verbose:$Verbose
-            
-        <### START ###>
-            if(($Global:XMLReader.Machine.Start.Enabled -eq "true") -and ($StartScript) -and ![string]::IsNullOrEmpty($Global:XMLReader.Machine.Start)) 
-            {
-                if($Global:XMLReader.Machine.Start.ClearHost -eq "true"){Clear-Host;} # clears host from the progess text
-    
-                # Some output methods that should be defined
-                # Greetings, calendar
-    
-                # Greetings
-                if(![string]::IsNullOrEmpty($Global:XMLReader.Machine.Start.Greetings))
-                {
-                    if([String]::IsNullOrEmpty($Global:XMLReader.Machine.Start.Greetings.Type)){[String]$Type = "Big";}
-                    else{[String]$Type = $Global:XMLReader.Machine.Start.Greetings.Type;}
-                    [System.Boolean]$Save = [System.Boolean]$Global:XMLReader.Machine.Start.Greetings.Save;
-                    $arg = 
-                    @{
-                        string=$Global:XMLReader.Machine.Start.Greetings.InnerXml; # String 
-                        Type=$Type; # Type of font
-                        SaveToFile=$Save;
-                        UniqueExtension=$("." + $($Global:AppPointer.Machine.ConfigFile | Split-Path -Leaf));
-                    };
-                    [String]$GreetingsPath = (Get-ChildItem $($Global:AppJson.Files.Greetings)).FullName; # Gets the full file path to the greetings script
-                    & $GreetingsPath @arg;
-                }
-    
-                # If the script is defined, run it
-                if(![String]::IsNullOrEmpty($Global:XMLReader.Machine.Start.Script) -and $(Test-Path $Global:XMLReader.Machine.Start.Script))
-                {
-                    Invoke-Expression $(Evaluate -value:$Global:XMLReader.Machine.Start.Script);# Executes the file that the user defines
-                } 
-            }
-        
-        try 
+    Push-Location $GitRepoDir; 
+
+    <# GET App Json, think of a better name for this #>
+    $AppJsonPath = $GitRepoDir + "\Config\app.json";
+    $Global:AppJson = [System.Object[]]$(Get-Content $AppJsonPath | ConvertFrom-Json);
+    # pwsh
+    # Todo test different environments
+    $pathToConfig = $GitRepoDir + $Global:AppJson.Directories.UserConfig + $Global:AppPointer.Machine.ConfigFile;
+    $Global:XMLReader = [System.Xml.XmlDocument]$(Get-Content $pathToConfig);
+
+    # Flag to load profile (in case someone wanting to use powershell)
+    if(!$Global:XMLReader.Machine.LoadProfile.ToBoolean($null))
+    {
+        break;
+    } 
+
+    # Helps debugging if on
+    if(($Global:XMLReader.Machine.LoadProcedure -eq "Verbose") -and !$Silent)
+    {
+        $Verbose = $true
+    } 
+    else
+    {
+        $Verbose = $false
+    }
+
+    Import-Module .\Modules\xProUtilities.psm1 -DisableNameChecking:$true -Scope Local -WarningAction SilentlyContinue;
+
+    <### LOGS ###>
+    $Global:LogHandler = Get-LogObject;
+
+    <### CHECK UPDATES ###>
+    if([string]::IsNullOrEmpty($BuildPath))
+    {
+        if((.\update-profile.ps1) -or (.\update-config.ps1 -CheckUpdate))
         {
-            if(![String]::IsNullOrEmpty($Global:XMLReader.Machine.ShellSettings.GitDisplay) -and ($Global:XMLReader.Machine.ShellSettings.Enabled.ToBoolean($null)) -and [string]::IsNullOrEmpty($BuildPath))
+            if("y" -eq $(Read-Host -Prompt "Do you want to restart powershell? (y/n)"))
             {
-                [String]$gitstring = "Version: $(git describe --tags)";
-                if($gitstring.Contains("-")){Write-Host "`n$($gitstring.Substring(0,$gitstring.IndexOf("-")))`n" -ForegroundColor Gray;}
-                else {Write-Host "`n$($gitstring)`n" -ForegroundColor Gray;}
+                if($PSVersionTable.PSVersion.Major -lt 7){Start-Process powershell;Stop-Process -Id $PID;}
+                else{Start-Process pwsh;Stop-Process -Id $PID;}
             }
+            else{exit;}
         }
-        catch 
+    }
+
+    <### GET CREDENTIALS ###>
+        CheckCredentials;
+    
+    <# BACKGROUND SETTINGS #>
+        # Doing this early so if Verbose:$false then progress color will be set
+        Import-Module .\Modules\Terminal.psm1 -DisableNameChecking:$true -Scope Local -WarningAction SilentlyContinue;
+        _SetBackgroundColor;
+        
+    <### PROGRAMS ###> 
+        LoadPrograms -XMLReader:$Global:XMLReader -Verbose:$Verbose
+    
+    <### MODULES ###>
+        LoadModules -XMLReader:$Global:XMLReader -Verbose:$Verbose
+        
+    <### OBJECTS ###>
+        LoadObjects -XMLReader:$Global:XMLReader -Verbose:$Verbose
+
+    <### FUNCTIONS ###>
+        LoadFunctions -XMLReader:$Global:XMLReader -Verbose:$Verbose
+        
+    <### START ###>
+        if(($Global:XMLReader.Machine.Start.Enabled -eq "true") -and ($StartScript) -and ![string]::IsNullOrEmpty($Global:XMLReader.Machine.Start)) 
         {
-            $LogHandler.Write("You may not have git bash installed.")
+            if($Global:XMLReader.Machine.Start.ClearHost -eq "true"){Clear-Host;} # clears host from the progess text
+
+            # Some output methods that should be defined
+            # Greetings, calendar
+
+            # Greetings
+            if(![string]::IsNullOrEmpty($Global:XMLReader.Machine.Start.Greetings))
+            {
+                if([String]::IsNullOrEmpty($Global:XMLReader.Machine.Start.Greetings.Type)){[String]$Type = "Big";}
+                else{[String]$Type = $Global:XMLReader.Machine.Start.Greetings.Type;}
+                [System.Boolean]$Save = [System.Boolean]$Global:XMLReader.Machine.Start.Greetings.Save;
+                $arg = 
+                @{
+                    string=$Global:XMLReader.Machine.Start.Greetings.InnerXml; # String 
+                    Type=$Type; # Type of font
+                    SaveToFile=$Save;
+                    UniqueExtension=$("." + $($Global:AppPointer.Machine.ConfigFile | Split-Path -Leaf));
+                };
+                [String]$GreetingsPath = (Get-ChildItem $($Global:AppJson.Files.Greetings)).FullName; # Gets the full file path to the greetings script
+                & $GreetingsPath @arg;
+            }
+
+            # If the script is defined, run it
+            if(![String]::IsNullOrEmpty($Global:XMLReader.Machine.Start.Script) -and $(Test-Path $Global:XMLReader.Machine.Start.Script))
+            {
+                Invoke-Expression $(Evaluate -value:$Global:XMLReader.Machine.Start.Script);# Executes the file that the user defines
+            } 
         }
+    
+    try 
+    {
+        if(![String]::IsNullOrEmpty($Global:XMLReader.Machine.ShellSettings.GitDisplay) -and ($Global:XMLReader.Machine.ShellSettings.Enabled.ToBoolean($null)) -and [string]::IsNullOrEmpty($BuildPath))
+        {
+            [String]$gitstring = "Version: $(git describe --tags)";
+            if($gitstring.Contains("-")){Write-Host "`n$($gitstring.Substring(0,$gitstring.IndexOf("-")))`n" -ForegroundColor Gray;}
+            else {Write-Host "`n$($gitstring)`n" -ForegroundColor Gray;}
+        }
+    }
+    catch 
+    {
+        $LogHandler.Write("You may not have git bash installed.")
+    }
     
     Pop-Location;
     

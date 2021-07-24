4a24229f5291914c41c4f936f40a08a562bdadf7
BrandonMFong <fong.m.brandon97@gmail.com>
2021-07-24 14:16:38 -0700

    Reorganizing

--- sources/testProject/src/testProject.cpp
+++ sources/testProject/src/testProject.cpp
@@ -1,6 +1,6 @@
 //============================================================================
-// Name        : testproject.cpp
-// Author      : Brando
+// Name        : testProject.cpp
+// Author      : 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style

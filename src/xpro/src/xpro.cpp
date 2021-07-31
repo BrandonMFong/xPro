1de3854f42989b4bef43f09a3b77e3fe3b750a9f
BrandonMFong <fong.m.brandon97@gmail.com>
2021-07-31 11:37:58 -0700

    Saving

--- src/xpro/src/xpro.cpp
+++ src/xpro/src/xpro.cpp
@@ -19,10 +19,26 @@
 #define kPipename "\\\\.\\pipe\\mynamedpipe"
 
 int main() {
-   BOOL   fConnected = FALSE;
-   DWORD  dwThreadId = 0;
-   HANDLE hPipe = INVALID_HANDLE_VALUE;
-   HANDLE hThread = NULL;
-   LPCTSTR lpszPipename = kPipename;
+	xError error = kNoError;
+	BOOL   connected = FALSE;
+	DWORD  dwThreadId = 0;
+	HANDLE pipeHandler = INVALID_HANDLE_VALUE;
+	HANDLE hThread = NULL;
+	LPCTSTR lpszPipename = kPipename;
 
+	if (error == kNoError) {
+		while (true) {
+			pipeHandler = CreateNamedPipe(
+			          lpszPipename,             // pipe name
+			          PIPE_ACCESS_DUPLEX,       // read/write access
+			          PIPE_TYPE_MESSAGE |       // message type pipe
+			          PIPE_READMODE_MESSAGE |   // message-read mode
+			          PIPE_WAIT,                // blocking mode
+			          PIPE_UNLIMITED_INSTANCES, // max. instances
+			          BUFSIZE,                  // output buffer size
+			          BUFSIZE,                  // input buffer size
+			          0,                        // client time-out
+			          NULL);
+		}
+	}
 }

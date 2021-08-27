5db07c22214806ba77e79563f3554ba27b8b7367
BrandonMFong <fong.m.brandon97@gmail.com>
2021-08-26 21:34:24 -0700

    Cleaning up

--- src/xpro/src/xpro.cpp
+++ src/xpro/src/xpro.cpp
@@ -20,16 +20,12 @@
 #define kBufferSize 512
 #define kPipename "\\\\.\\pipe\\mynamedpipe"
 
-long unsigned int InstanceThread(void * param);
-
 xError SendResponse(HANDLE pipe);
 
 int main() {
-	xError 				error 			= kNoError;
-//	long unsigned int  	threadID 		= 0;
-	HANDLE 				pipeHandler 	= INVALID_HANDLE_VALUE;
-//	HANDLE 				threadHandler 	= NULL;
-	const char * 		pipeName 		= kPipename;
+	xError 			error 		= kNoError;
+	HANDLE 			pipeHandler = INVALID_HANDLE_VALUE;
+	const char *	pipeName 	= kPipename;
 
 	while (error == kNoError) {
 		if (error == kNoError) {
@@ -76,13 +72,13 @@ int main() {
 
 xError SendResponse(HANDLE pipe)
 {
-	xError result = kNoError;
-	char * readBuffer = NULL;
-	char * writeBuffer = NULL;
-	bool success = false;
-	long unsigned int bytesRead = 0;
-	long unsigned int messageSize = 0;
-	long unsigned int bytesWritten = 0;
+	xError 				result 			= kNoError;
+	char * 				readBuffer 		= NULL;
+	char * 				writeBuffer 	= NULL;
+	bool 				success 		= false;
+	long unsigned int 	bytesRead 		= 0;
+	long unsigned int 	messageSize 	= 0;
+	long unsigned int 	bytesWritten 	= 0;
 
 	if (result == kNoError) {
 		result = pipe != NULL ? kNoError : kPipeError;
@@ -138,78 +134,4 @@ xError SendResponse(HANDLE pipe)
 	}
 
 	return result;
-}
-
-long unsigned int InstanceThread(void * param) {
-	xError 				error 			= kNoError;
-	char * 				readBuffer 		= NULL;
-	char * 				writeBuffer 	= NULL;
-	long unsigned int 	bytesRead 		= 0;
-	long unsigned int 	messageSize 	= 0;
-	long unsigned int 	bytesWritten 	= 0;
-	HANDLE 				pipeHandler 	= NULL;
-	bool 				success 		= false;
-
-	if (error == kNoError) {
-		error = param != NULL ? kNoError : kNULLError;
-	}
-
-	if (error == kNoError) {
-		pipeHandler = (HANDLE) param;
-		error 		= pipeHandler != NULL ? kNoError : kPipeError;
-	}
-
-	if (error == kNoError) {
-		readBuffer 	= (char *) malloc(sizeof(char) * kBufferSize);
-		error 		= readBuffer != NULL ? kNoError : kNULLError;
-	}
-
-	if (error == kNoError) {
-		writeBuffer = (char *) malloc(sizeof(char) * kBufferSize);
-		error 		= writeBuffer != NULL ? kNoError : kNULLError;
-	}
-
-	while (error == kNoError) {
-		if (error == kNoError) {
-			success = ReadFile(
-				pipeHandler,
-				readBuffer,
-				kBufferSize * sizeof(char),
-				&bytesRead,
-				NULL
-			);
-
-			error = (success && (bytesRead != 0)) ? kNoError : kReadError;
-		}
-
-		if (error == kNoError) {
-			strcpy(writeBuffer, "Hello world! I did it!");
-			error = !strcmp(writeBuffer, "Hello world! I did it!") ? kNoError : kStringError;
-		}
-
-		if (error == kNoError) {
-			messageSize = (lstrlen(writeBuffer)+1) * sizeof(char);
-
-		    success = WriteFile(
-				pipeHandler,
-				writeBuffer,
-				messageSize,
-				&bytesWritten,
-				NULL
-			);
-
-		    error = success ? kNoError : kWriteError;
-		}
-	}
-
-	FlushFileBuffers(pipeHandler);
-	DisconnectNamedPipe(pipeHandler);
-	CloseHandle(pipeHandler);
-
-	free(readBuffer);
-	free(writeBuffer);
-
-	DLog("InstanceThread exiting.\n");
-
-	return 1;
 }

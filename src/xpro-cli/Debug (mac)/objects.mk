8c18ee32a43ca8d169d53a9b54e1b1f1a98c3970
brandonmfong <fong.m.brandon97@gmail.com>
2021-10-19 20:23:54 -0700

    Changing dir

--- src/xpro-cli/Debug (mac)/objects.mk
+++ src/xpro-cli/Debug (mac)/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := /Users/brandonmfong/brando/2021/sources/repo/xPro/lib/c++/Debug\ (mac)/libxLib.a
+USER_OBJS := /Users/brandonmfong/brando/sources/repo/xPro/lib/c++/Debug\ (mac)/libxLib.a
 
 LIBS :=
 

ec4b9a0a8a6a4d184bd981cc484952ff86f4c7a9
brandonmfong <fong.m.brandon97@gmail.com>
2021-10-27 21:11:04 -0700

    Inserting os macros

--- src/xpro-cli/Debug (mac)/src/main.d
+++ src/xpro-cli/Debug (mac)/src/main.d
@@ -3,11 +3,11 @@ src/main.o: ../src/main.cpp \
   /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xPipe.h \
   /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xError.h \
   /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xLogger/xDLog.h \
-  /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xIntType.h \
+  /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xInt.h \
   /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xNull.h \
   /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xBool.h \
   /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xUtilities/xUtilities.h \
-  ../src/xArgs.h
+  /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xArguments/xArgs.h
 
 /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xLib.h:
 
@@ -17,7 +17,7 @@ src/main.o: ../src/main.cpp \
 
 /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xLogger/xDLog.h:
 
-/Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xIntType.h:
+/Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xInt.h:
 
 /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xNull.h:
 
@@ -25,4 +25,4 @@ src/main.o: ../src/main.cpp \
 
 /Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xUtilities/xUtilities.h:
 
-../src/xArgs.h:
+/Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib/xArguments/xArgs.h:

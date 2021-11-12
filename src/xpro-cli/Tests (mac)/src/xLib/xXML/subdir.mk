eab528c68859ec0b635b41dfd0d75d82a371958c
brandonmfong <fong.m.brandon97@gmail.com>
2021-11-11 20:36:09 -0800

    Adding test files

---
+++ src/xpro-cli/Tests (mac)/src/xLib/xXML/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/xLib/xXML/xXML.cpp 
+
+OBJS += \
+./src/xLib/xXML/xXML.o 
+
+CPP_DEPS += \
+./src/xLib/xXML/xXML.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/xLib/xXML/%.o: ../src/xLib/xXML/%.cpp src/xLib/xXML/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DDEBUG -DTESTING -D__MACOS__ -I"/Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src/xLib" -I"/Users/brandonmfong/brando/sources/repo/xPro/src/xpro-cli/src" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+

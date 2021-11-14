b7db4507301b88cea01b1242e56972daea687cbc
brandonmfong <fong.m.brandon97@gmail.com>
2021-11-14 10:59:17 -0800

    leaving brackets white

--- src/xpro-cli/src/xLib/xTests.h
+++ src/xpro-cli/src/xLib/xTests.h
@@ -32,8 +32,8 @@
 
 #define INTRO_TEST_FUNCTION printf("Testing %s:\n", __func__)
 #define PRINT_TEST_RESULTS(result) \
-	if (result) {system("printf \"\033[0;32m[ Pass ] \033[0m\"");}\
-	else {system("printf \"\033[0;31m[ Fail ] \033[0m\"");}\
+	if (result) {system("printf \"[\033[0;32m Pass \033[0m] \"");}\
+	else {system("printf \"[\033[0;31m Fail \033[0m] \"");}\
 	printf("%s\n", __func__)
 
 #else // defined(TESTING)

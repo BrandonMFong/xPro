cdb2bea6c93d83c2919ec292eb6faf7e11ee75c5
Brandon Fong <bfong@owc.com>
2021-11-15 18:20:10 -0800

    Adding new files to build

--- src/xpro-cli/Debug (mac)/src/Utilities/subdir.mk
+++ src/xpro-cli/Debug (mac)/src/Utilities/subdir.mk
@@ -4,17 +4,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/AppDriver/Utilities/Utilities.c 
+../src/Utilities/Utilities.c 
 
 OBJS += \
-./src/AppDriver/Utilities/Utilities.o 
+./src/Utilities/Utilities.o 
 
 C_DEPS += \
-./src/AppDriver/Utilities/Utilities.d 
+./src/Utilities/Utilities.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/AppDriver/Utilities/%.o: ../src/AppDriver/Utilities/%.c src/AppDriver/Utilities/subdir.mk
+src/Utilities/%.o: ../src/Utilities/%.c src/Utilities/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
 	gcc -DDEBUG -D__MACOS__ -I$(SOURCE_PATH)/src/xLib -I$(SOURCE_PATH)/src -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"

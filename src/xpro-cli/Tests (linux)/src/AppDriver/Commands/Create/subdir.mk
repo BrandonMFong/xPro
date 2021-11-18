a2c5f934eb2690f84e437f2ecf4bd7d761c71e9c
brandonmfong <fong.m.brandon97@gmail.com>
2021-11-17 22:30:16 -0800

    introducing linux tests

---
+++ src/xpro-cli/Tests (linux)/src/AppDriver/Commands/Create/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/AppDriver/Commands/Create/Create.cpp 
+
+OBJS += \
+./src/AppDriver/Commands/Create/Create.o 
+
+CPP_DEPS += \
+./src/AppDriver/Commands/Create/Create.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/AppDriver/Commands/Create/%.o: ../src/AppDriver/Commands/Create/%.cpp src/AppDriver/Commands/Create/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DDEBUG -DTESTING -D__LINUX__ -I$(SOURCE_PATH)/src -I$(SOURCE_PATH)/src/xLib -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ src/xpro-cli/Tests (linux)/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/xMain.cpp \
+../src/xTests.cpp 
+
+OBJS += \
+./src/xMain.o \
+./src/xTests.o 
+
+CPP_DEPS += \
+./src/xMain.d \
+./src/xTests.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DDEBUG -DTESTING -D__LINUX__ -I$(SOURCE_PATH)/src -I$(SOURCE_PATH)/src/xLib -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+

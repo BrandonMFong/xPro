e09b26b90691db9721c3c833a38ebf0631f21a75
BrandonMFong <fong.m.brandon97@gmail.com>
2021-11-23 21:03:20 -0800

    not defining mingw macro

--- src/xpro-cli/Debug (windows)/src/subdir.mk
+++ src/xpro-cli/Debug (windows)/src/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp src/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -DDEBUG -D__WINDOWS__ -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src\xLib" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli" -I"/src" -I"/src/xLib" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src\xLib" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	g++ -DDEBUG -D__WINDOWS__ -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src\xLib" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli" -I"/src" -I"/src/xLib" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src\xLib" -O0 -g3 -Wall -c -fmessage-length=0 -Wno-unknown-pragmas -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- src/xpro-cli/Debug (windows)/src/xLib/xUtilities/subdir.mk
+++ src/xpro-cli/Debug (windows)/src/xLib/xUtilities/subdir.mk
@@ -23,7 +23,7 @@ C_DEPS += \
 src/xLib/xUtilities/%.o: ../src/xLib/xUtilities/%.c src/xLib/xUtilities/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -DDEBUG -D__WINDOWS__ -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src\xLib" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli" -I"/src" -I"/src/xLib" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src\xLib" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	gcc -DDEBUG -D__WINDOWS__ -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src\xLib" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli" -I"/src" -I"/src/xLib" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src" -I"B:\SOURCE\Repo\xpro-projects\xpro\src\xpro-cli\src\xLib" -O0 -g3 -Wall -c -fmessage-length=0 -Wno-unknown-pragmas -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

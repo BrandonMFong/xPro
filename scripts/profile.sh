56d4fe18489f1f1f3ba38c66f770949822ed11bb
brandonmfong <fong.m.brandon97@gmail.com>
2021-11-25 22:24:31 -0800

    Pushing to location

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -6,6 +6,8 @@ result=0;
 xproPath="$( cd -- "$(dirname -- "$0")" >/dev/null 2>&1 ; pwd -P )";
 xutilPath="xutil.sh";
 
+pushd $xproPath >/dev/null 2>&1;
+
 if [ ! -d $xproPath ]; then 
     result = 1;
     printf "$xproPath does not exist\n";
@@ -17,6 +19,8 @@ if [ $result -eq 0 ]; then
     source $xutilPath;
     result=$?;
 fi 
+
+popd >/dev/null 2>&1; # $xproPath
 
 if [ $result -eq 0 ]; then 
     printf "Successfully loaded xpro\n"

9d585da2e9660ec385a2ab2617c91c7db5999ca5
brandonmfong <fong.m.brandon97@gmail.com>
2021-11-25 20:42:57 -0800

    Renaming to just build

--- scripts/build.py
+++ scripts/build.py
@@ -10,32 +10,38 @@ import os
 import subprocess
 import shutil
 
+## CONSTANTS START ##
+
+BUILD_FOLDER: str = ""
+SCRIPT_PATH: str = os.path.realpath(os.path.dirname(sys.argv[0]))
+XPRO_PATH: str = os.path.dirname(SCRIPT_PATH)
+BUILD_PATH: str = "{}/src/xpro-cli/Debug (mac)".format(XPRO_PATH)
+BIN_PATH: str = "{}/bin".format(XPRO_PATH)
+XP_BUILD: str = "debug-xp"
+
+## CONSTANTS END ##
+
 ## VARIABLES START ##
 
-status: int = 0
-scriptPath: str = os.path.realpath(os.path.dirname(sys.argv[0]))
-xproPath: str = os.path.dirname(scriptPath)
-xproCLISourcePath: str = "{}/src/xpro-cli/Debug (mac)".format(xproPath)
-binPath: str = "{}/bin".format(xproPath)
-currDir: str = os.curdir
-xpBuild: str = "debug-xp"
+status:     int = 0
+currDir:    str = os.curdir
 
 ## VARIABLES END ##
 
 ## START ##
 
 # Change xpro
-os.chdir(xproPath)
+os.chdir(XPRO_PATH)
 
-if os.path.exists(binPath) is False:
-    os.mkdir(binPath)
+if os.path.exists(BIN_PATH) is False:
+    os.mkdir(BIN_PATH)
 
-    if os.path.exists(binPath) is False:
+    if os.path.exists(BIN_PATH) is False:
         status = 1
         print("Could not create bin folder")
 
 # Change to source directory
-os.chdir(xproCLISourcePath)
+os.chdir(BUILD_PATH)
 
 # Clean
 if status == 0:
@@ -54,12 +60,12 @@ if status == 0:
     ).wait()
 
 if status == 0:
-    path = "{}/{}".format(binPath, xpBuild)
+    path = "{}/{}".format(BIN_PATH, XP_BUILD)
     if os.path.exists(path):
         print("Removing old build:", path)
         os.remove(path)
 
-    path = shutil.copy(xpBuild, binPath)
+    path = shutil.copy(XP_BUILD, BIN_PATH)
     if path is None:
         status = 1
     else:

ba5bab5d55ada0da467fa7a4dbd964adb3210451
BrandonMFong <fong.m.brandon97@gmail.com>
2021-11-27 01:09:14 -0800

    Modifying profile to load correctly

--- scripts/profile.ps1
+++ scripts/profile.ps1
@@ -3,11 +3,28 @@
     xPro Microsoft Powershell profile
 #>
 
-using module .\modules\xError.psm1
+$xproPath = $env:HOMEDRIVE + $env:HOMEPATH + "\.xpro";
+$success = (Test-Path -Path $xproPath);
 
-[xError]$error = [xError]::kNoError;
+Push-Location $xproPath;
 
-if ($error -eq [xError]::kNoError) {
-    Write-Host "NoError!"
+# If path exists then add path to path
+if ($success) {
+    $env:Path = $xproPath + ";" + $env:Path;
+
+} else {
+    Write-Warning "Path $xproPath does not exist!";
 }
 
+if ($success) {
+    Import-Module $xproPath\xutil.psm1 -Scope Global;
+    $success = $?;
+}
+
+Pop-Location;
+
+if ($success) {
+    Write-Host "Successfully loaded xPro";
+} else {
+    Write-Warning "Failed to load xPro";
+}
--- scripts/xutil.psm1
+++ scripts/xutil.psm1
@@ -1,1 +1,34 @@
 
+function goto {
+    param (
+        [Parameter (Mandatory = $true)] [String] $alias
+    )
+    $success = $true;
+    $xpPath = $null;
+    $path = $null;
+
+    # make sure the .xpro path is set
+    try {
+        $xpPath = (Get-Command xp.exe).Path
+    }
+    catch {
+        $success = $false;
+        Write-Warning "xp binary could not be found";
+    }
+
+    # Get path for the alias
+    if ($success) {
+        $path = (& $xpPath dir $alias);
+        $success = $?;
+    }
+
+    # make sure that path exists
+    if ($success) {
+        $success = (Test-Path -Path $path);
+    }
+
+    # Set location
+    if ($success) {
+        Set-Location $path;
+    }
+}

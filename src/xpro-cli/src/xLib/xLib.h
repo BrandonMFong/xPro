242022e07120d7db7694bf32c564084a0f6f51e7
Brandon Fong <fong.m.brandon97@gmail.com>
2022-03-01 21:24:29 -0800

    Reorganizing

--- src/xpro-cli/src/xLib/xLib.h
+++ src/xpro-cli/src/xLib/xLib.h
@@ -8,6 +8,8 @@
 #ifndef XLIB_H_
 #define XLIB_H_
 
+#pragma mark - Headers
+
 /// Standard lib
 #include <libgen.h>
 
@@ -29,5 +31,27 @@
 #include <xXML/xXML.hpp>
 
 #endif // __cplusplus
+
+#pragma mark - Macros
+
+/**
+ * If TESTING is defined, we need to have the entry point be the tests
+ */
+#if defined(TESTING)
+
+#define xMain 	xMain
+#define xTests 	main
+
+/**
+ * Define a test path and put path into this variable for unit tests to use
+ */
+static char testPath[MAX_PATH_LENGTH];
+
+#else
+
+#define xMain 	main
+#define xTests 	xTests
+
+#endif
 
 #endif /* XLIB_H_ */
--- src/xpro-cli/src/xLib/xTests.h
+++ src/xpro-cli/src/xLib/xTests.h
@@ -8,27 +8,7 @@
 #ifndef SRC_XLIB_XTESTS_H_
 #define SRC_XLIB_XTESTS_H_
 
-#include "xUtilities/xUtilities.h"
-
-/**
- * If TESTING is defined, we need to have the entry point be the tests
- */
-#if defined(TESTING)
-
-#define xMain 	xMain
-#define xTests 	main
-
-/**
- * Define a test path and put path into this variable for unit tests to use
- */
-static char testPath[MAX_PATH_LENGTH];
-
-#else
-
-#define xMain 	main
-#define xTests 	xTests
-
-#endif
+#include <xUtilities/xUtilities.h>
 
 /**
  * Use this to print test results.  It will also print the

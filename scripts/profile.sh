d92b8397a9af556cc1d01981a06b5f7e7c418f65
Brandon Fong <fong.m.brandon97@gmail.com>
2022-03-01 21:56:32 -0800

    Not exiting

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -14,8 +14,24 @@ result=0;
 
 ## VARIABLES END ##
 
+## FUNCTIONS START ##
+
+## brief: Creates bash/zsh variables
+##
+## returns:
+##  - Sets result
+function loadObjects() {
+
+    result=0;
+}
+
+## FUNCTIONS END ##
+
+## MAIN START ##
+
 pushd $XPRO_PATH >/dev/null 2>&1;
 
+# Add xpro to path 
 if [ ! -d $XPRO_PATH ]; then 
     result = 1;
     printf "$XPRO_PATH does not exist\n";
@@ -23,15 +39,24 @@ else
     PATH=$PATH:$XPRO_PATH;
 fi 
 
+# Acquire functions and variables from 
+# utility script
 if [ $result -eq 0 ]; then 
     source $XUTIL_PATH;
     result=$?;
 fi 
 
+# Load variables
+if [ $result -eq 0 ]; then 
+    loadObjects;
+fi 
+
 popd >/dev/null 2>&1; # $XPRO_PATH
 
 if [ $result -eq 0 ]; then 
-    printf "Successfully loaded xpro\n"
+    printf "Successfully loaded xpro\n";
 else 
-    printf "Failed to load xpro\n"
+    printf "Failed to load xpro\n";
 fi 
+
+## MAIN END ##

709b2cf1ae6ef948051293eb0cca914926719bee
Brandon Fong <fong.m.brandon97@gmail.com>
2022-03-30 20:07:02 -0700

    Updating make files

---
+++ src/xpro-cli/Debug (mac)/src/AppDriver/Commands/Help/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/AppDriver/Commands/Help/Help.cpp 
+
+CPP_DEPS += \
+./src/AppDriver/Commands/Help/Help.d 
+
+OBJS += \
+./src/AppDriver/Commands/Help/Help.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/AppDriver/Commands/Help/%.o: ../src/AppDriver/Commands/Help/%.cpp src/AppDriver/Commands/Help/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DDEBUG -D__MACOS__ -I$(SOURCE_PATH)/src/xLib -I$(SOURCE_PATH)/src -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -DVERSION=$(VERSION_STRING) -DBUILD=$(BUILD_HASH) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src-2f-AppDriver-2f-Commands-2f-Help
+
+clean-src-2f-AppDriver-2f-Commands-2f-Help:
+	-$(RM) ./src/AppDriver/Commands/Help/Help.d ./src/AppDriver/Commands/Help/Help.o
+
+.PHONY: clean-src-2f-AppDriver-2f-Commands-2f-Help
+
---
+++ src/xpro-cli/Tests (windows)/src/AppDriver/Commands/Alias/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/AppDriver/Commands/Alias/Alias.cpp 
+
+CPP_DEPS += \
+./src/AppDriver/Commands/Alias/Alias.d 
+
+OBJS += \
+./src/AppDriver/Commands/Alias/Alias.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/AppDriver/Commands/Alias/%.o: ../src/AppDriver/Commands/Alias/%.cpp src/AppDriver/Commands/Alias/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DDEBUG -DTESTING -D__WINDOWS__ -I"$(SOURCE_PATH)\src\xLib" -I"$(SOURCE_PATH)\src" -O0 -g3 -Wall -c -fmessage-length=0 -DVERSION=$(VERSION_STRING) -DBUILD=$(BUILD_HASH) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src-2f-AppDriver-2f-Commands-2f-Alias
+
+clean-src-2f-AppDriver-2f-Commands-2f-Alias:
+	-$(RM) ./src/AppDriver/Commands/Alias/Alias.d ./src/AppDriver/Commands/Alias/Alias.o
+
+.PHONY: clean-src-2f-AppDriver-2f-Commands-2f-Alias
+

0a2a51f9b2ce574122f9ff62246837bccaba8437
Brandon Fong <fong.m.brandon97@gmail.com>
2022-03-31 18:55:00 -0700

    Grouping logic into a function

--- build.py
+++ build.py
@@ -75,91 +75,99 @@ def help():
         help        = HELP_ARG
     ))
 
-def main():
-    status:             int = 0
+def build():
+    result:             int = 0
     currDir:            str = os.curdir
     buildTypeString:    str = ""
     buildFolder:        str = ""
     buildPath:          str = ""
     buildName:          str = XP_BUILD
 
+    if result == 0:
+        if (DEBUG_ARG in sys.argv) and (RELEASE_ARG in sys.argv):
+            print("Please choose either '{}' or '{}'".format(
+                DEBUG_ARG, RELEASE_ARG
+            ))
+            result = 1
+        elif DEBUG_ARG in sys.argv:
+            buildTypeString = "Debug"
+            buildName       = "debug-{}".format(buildName)
+        elif RELEASE_ARG in sys.argv:
+            buildTypeString = "Release"
+        else:
+            print("No option provided")
+            print("Please see '{} {}' for help".format(
+                SCRIPT_NAME, HELP_ARG
+            ))
+            result = 1
+
+    # Make sure we have the path to build
+    if result == 0:
+        if os.path.exists(XPRO_LINK_PATH) is False:
+            result = 1
+            print("We need to have '{}' to build. Please run '{}'".format(
+                XPRO_LINK_PATH,
+                DEVENV_SCRIPT
+            ))
+    
+    # Change xpro
+    os.chdir(XPRO_PATH)
+
+    if result == 0:
+        if os.path.exists(BIN_PATH) is False:
+            os.mkdir(BIN_PATH)
+
+            if os.path.exists(BIN_PATH) is False:
+                result = 1
+                print("Could not create bin folder")
+    
+    if result == 0:
+        buildFolder = "{} ({})".format(buildTypeString, PLATFORM_NAME)
+        buildPath   = os.path.join(XPRO_PATH, "src", "xpro-cli", buildFolder)
+
+        # Change to source directory
+        os.chdir(buildPath)
+
+        # Clean
+        print("Cleaning up xPro CLI")
+        result = subprocess.Popen(
+            [ "make", "clean" ], 
+            stderr = subprocess.STDOUT
+        ).wait()
+        
+    # Build all
+    if result == 0:
+        print("Building xPro CLI")
+        result = subprocess.Popen(
+            [ "make", "all" ], 
+            stderr = subprocess.STDOUT
+        ).wait()
+
+    if result == 0:
+        path = os.path.join(BIN_PATH, buildName)
+        if os.path.exists(path):
+            print("Removing old build:", path)
+            os.remove(path)
+
+        path = shutil.copy(buildName, BIN_PATH)
+        if path is None:
+            result = 1
+        else:
+            print("Build copy:", path)
+
+    # Go back to dir
+    os.chdir(currDir)
+
+    return result
+
+def main():
+    status: int = 0
+
     if HELP_ARG in sys.argv:
         help()
     else:
         if status == 0:
-            if (DEBUG_ARG in sys.argv) and (RELEASE_ARG in sys.argv):
-                print("Please choose either '{}' or '{}'".format(
-                    DEBUG_ARG, RELEASE_ARG
-                ))
-                status = 1
-            elif DEBUG_ARG in sys.argv:
-                buildTypeString = "Debug"
-                buildName       = "debug-{}".format(buildName)
-            elif RELEASE_ARG in sys.argv:
-                buildTypeString = "Release"
-            else:
-                print("No option provided")
-                print("Please see '{} {}' for help".format(
-                    SCRIPT_NAME, HELP_ARG
-                ))
-                status = 1
-
-        # Make sure we have the path to build
-        if status == 0:
-            if os.path.exists(XPRO_LINK_PATH) is False:
-                status = 1
-                print("We need to have '{}' to build. Please run '{}'".format(
-                    XPRO_LINK_PATH,
-                    DEVENV_SCRIPT
-                ))
-        
-        # Change xpro
-        os.chdir(XPRO_PATH)
-
-        if status == 0:
-            if os.path.exists(BIN_PATH) is False:
-                os.mkdir(BIN_PATH)
-
-                if os.path.exists(BIN_PATH) is False:
-                    status = 1
-                    print("Could not create bin folder")
-        
-        if status == 0:
-            buildFolder = "{} ({})".format(buildTypeString, PLATFORM_NAME)
-            buildPath   = os.path.join(XPRO_PATH, "src", "xpro-cli", buildFolder)
-
-            # Change to source directory
-            os.chdir(buildPath)
-
-            # Clean
-            print("Cleaning up xPro CLI")
-            status = subprocess.Popen(
-                [ "make", "clean" ], 
-                stderr = subprocess.STDOUT
-            ).wait()
-            
-        # Build all
-        if status == 0:
-            print("Building xPro CLI")
-            status = subprocess.Popen(
-                [ "make", "all" ], 
-                stderr = subprocess.STDOUT
-            ).wait()
-
-        if status == 0:
-            path = os.path.join(BIN_PATH, buildName)
-            if os.path.exists(path):
-                print("Removing old build:", path)
-                os.remove(path)
-
-            path = shutil.copy(buildName, BIN_PATH)
-            if path is None:
-                status = 1
-            else:
-                print("Build copy:", path)
-
-        # Go back to dir
-        os.chdir(currDir)
+            status = build()
 
     sys.exit(status)
 

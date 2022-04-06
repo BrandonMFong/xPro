166236448ed1d70eb70a169d91028e57f8869346
Brandon Fong <fong.m.brandon97@gmail.com>
2022-04-05 22:05:12 -0700

    Getting only 40 characters

--- _uuid.py
+++ _uuid.py
@@ -8,4 +8,4 @@ Helps create build hash for build process
 import hashlib
 import random 
 
-print(hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest())
+print(hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()[0:40])

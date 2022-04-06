62e33595bcb2998eaa9ee19fb8be0e11413bc717
BrandonMFong <fong.m.brandon97@gmail.com>
2022-04-05 21:51:25 -0700

    making uuid

---
+++ _uuid.py
@@ -0,0 +1,11 @@
+#!//usr/bin/python3
+"""
+_uuid.py
+======================
+Helps create build hash for build process
+"""
+
+import hashlib
+import random 
+
+print(hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest())

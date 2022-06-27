ce86c5c0e06b3cd6bd4fc60c8440fcef6dd250b0
Brandon Fong <fong.m.brandon97@gmail.com>
2022-06-27 10:03:07 -0700

    updating makefiles

--- src/xpro-cli/Tests (windows)/src/xLib/xXML/subdir.mk
+++ src/xpro-cli/Tests (windows)/src/xLib/xXML/subdir.mk
@@ -4,15 +4,12 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/xLib/xXML/xXML-Parsing.cpp \
 ../src/xLib/xXML/xXML.cpp 
 
 CPP_DEPS += \
-./src/xLib/xXML/xXML-Parsing.d \
 ./src/xLib/xXML/xXML.d 
 
 OBJS += \
-./src/xLib/xXML/xXML-Parsing.o \
 ./src/xLib/xXML/xXML.o 
 
 
@@ -28,7 +25,7 @@ src/xLib/xXML/%.o: ../src/xLib/xXML/%.cpp src/xLib/xXML/subdir.mk
 clean: clean-src-2f-xLib-2f-xXML
 
 clean-src-2f-xLib-2f-xXML:
-	-$(RM) ./src/xLib/xXML/xXML-Parsing.d ./src/xLib/xXML/xXML-Parsing.o ./src/xLib/xXML/xXML.d ./src/xLib/xXML/xXML.o
+	-$(RM) ./src/xLib/xXML/xXML.d ./src/xLib/xXML/xXML.o
 
 .PHONY: clean-src-2f-xLib-2f-xXML
 
